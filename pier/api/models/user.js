396c0ca2dd62d5815fb03e16964562cb443ea9c6
Brendan Kennedy <brenwken@gmail.com>
2019-11-22 00:13:57 -0500

    Remove hashing methods

--- pier/api/models/user.js
+++ pier/api/models/user.js
@@ -1,22 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   _id: mongoose.Types.ObjectId,
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
-
-// hash the password
-userSchema.methods.generateHash = function(password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-};
 
 const User = mongoose.model('User', userSchema);
 
