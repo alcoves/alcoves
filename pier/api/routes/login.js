47b0b057327b4274c9e27b88d2fd920aaccdc4ca
Brendan Kennedy <brenwken@gmail.com>
2019-11-22 01:06:15 -0500

    Cleanup unused routes

--- pier/api/routes/login.js
+++
@@ -1,9 +0,0 @@
-app.post('/login', function(req, res) {
-  User.findOne({ username: req.body.username }, function(err, user) {
-    if (!user.validPassword(req.body.password)) {
-      //password did not match
-    } else {
-      // password matched. proceed forward
-    }
-  });
-});
--- pier/api/routes/register.js
+++
@@ -1,8 +0,0 @@
-app.post('/register', function(req, res) {
-  var new_user = new User({
-    username: req.username,
-  });
-
-  new_user.password = new_user.generateHash(req.body.password);
-  new_user.save();
-});
