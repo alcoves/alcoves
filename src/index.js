9baef18ff118e3fedb7a5ba32153f0a9cc92adfb
Brendan Kennedy <brenwken@gmail.com>
2019-11-24 19:56:29 -0500

    Add routing to index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import theme from './config/theme';
 import TopBar from './components/TopBar/TopBar';
 import Content from './components/Content/Content';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 
 const Index = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <TopBar />
-      <Content />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <TopBar />
+        <Switch>
+          <Route path='/login' render={props => <SignUp {...props} />} />
+          <Route path='/' render={props => <Content {...props} />} />
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
