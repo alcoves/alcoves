fd1b8dd54d84dbd0eebfdd1b55f41185a1681c06
Brendan Kennedy <brenwken@gmail.com>
2019-11-29 12:07:47 -0500

    Make App component functional

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,59 +10,23 @@ import { Route, Switch } from 'react-router-dom';
 import { observer } from 'mobx-react';
 import { withCookies } from 'react-cookie';
 
-@observer
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      loading: false,
-    };
-  }
-
-  componentDidMount() {
-    const accessToken = this.props.cookies.get('accessToken');
+export default observer(
+  withCookies(props => {
+    const accessToken = props.cookies.get('accessToken');
 
     if (accessToken) {
-      console.log('accessToken present');
-
       const { payload } = jwt.decode(accessToken, { complete: true });
-
-      console.log(payload);
-
-      // this.props.stores.user.id = payload.userId;
-      // this.props.stores.user.email = payload.email;
-      // console.log(this.props.stores.user.id);
-
-      this.setState({ loading: false });
-    } else {
-      console.log('no accessToken present');
-      this.setState({ loading: false });
-    }
-  }
-
-  render() {
-    if (this.state.error) {
-      return <div> {this.state.error} </div>;
+      console.log('accessToken present', payload);
     }
 
-    if (this.state.loading) {
-      return <div />;
-    } else {
-      return (
-        <div>
-          <TopBar {...this.props} />
-          <Switch>
-            <Route
-              path='/login'
-              render={routerProps => <Login {...routerProps} {...this.props} />}
-            />
-            <Route path='/' render={routerProps => <Content {...routerProps} {...this.props} />} />
-          </Switch>
-        </div>
-      );
-    }
-  }
-}
-
-export default withCookies(Root);
+    return (
+      <div>
+        <TopBar {...props} />
+        <Switch>
+          <Route path='/login' render={routerProps => <Login {...routerProps} {...props} />} />
+          <Route path='/' render={routerProps => <Content {...routerProps} {...props} />} />
+        </Switch>
+      </div>
+    );
+  }),
+);
