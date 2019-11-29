fa95df7fe7bb1c6c4e3b59f7857f9894e135ea2f
Brendan Kennedy <brenwken@gmail.com>
2019-11-29 12:07:56 -0500

    Rework topbar icon

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import BkenIcon from '../../../public/favicon.ico';
 
 import { useHistory } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
@@ -31,24 +32,18 @@ export default observer(() => {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu">
+          <IconButton edge='start' className={classes.menuButton} color='inherit' aria-label='menu'>
             <MenuIcon />
           </IconButton>
-          Welcome
-          <Typography
-            variant="h6"
+          <div
             className={classes.title}
             onClick={() => {
               history.push('/');
             }}>
-            bken
-          </Typography>
+            <img style={{ cursor: 'pointer' }} src={BkenIcon} width={30} height={30} />
+          </div>
           {user.id ? (
             <div> {user.email} </div>
           ) : (
@@ -56,7 +51,7 @@ export default observer(() => {
               onClick={() => {
                 history.push('/login');
               }}
-              color="inherit">
+              color='inherit'>
               Login
             </Button>
           )}
