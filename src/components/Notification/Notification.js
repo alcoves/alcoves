7a4e401ab5f736e64ebda2b3b51211bfb4f6d249
Brendan Kennedy <brenwken@gmail.com>
2019-11-29 13:01:57 -0500

    Added basic notification component

---
+++ src/components/Notification/Notification.js
@@ -0,0 +1,107 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import { amber, green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { makeStyles } from '@material-ui/core/styles';
+import NotificationStore from '../../data/Notification';
+import { observer } from 'mobx-react-lite';
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
+};
+
+const useStyles1 = makeStyles(theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+
+function MySnackbarContentWrapper(props) {
+  const classes = useStyles1();
+  const { className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant], className)}
+      aria-describedby='client-snackbar'
+      message={
+        <span id='client-snackbar' className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton key='close' aria-label='close' color='inherit' onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+
+MySnackbarContentWrapper.propTypes = {
+  className: PropTypes.string,
+  message: PropTypes.string,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['error', 'info', 'success', 'warning']).isRequired,
+};
+
+export default observer(() => {
+  const { notification } = useContext(NotificationStore);
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') return;
+    notification.open = false;
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: notification.vertical || 'bottom',
+          horizontal: notification.horizontal || 'right',
+        }}
+        open={notification.open}
+        autoHideDuration={notification.duration || 5000}
+        onClose={handleClose}>
+        <MySnackbarContentWrapper
+          onClose={handleClose}
+          variant={notification.variant || 'info'}
+          message={notification.message || ''}
+        />
+      </Snackbar>
+    </div>
+  );
+});
