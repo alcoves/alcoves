aa0eae6918417668a5ab1212fbc87e938c8a419e
rustyguts <brendan.kennedy@skyspecs.com>
2019-12-19 14:03:10 -0500

    Hashrouter test

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const Index = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
