7458cabb311446ce1b2fb479b7b145aff1e42d13
Brendan Kennedy <brenwken@gmail.com>
2019-12-19 02:21:27 -0500

    Cleaning up video page

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -22,30 +22,20 @@ export default observer(() => {
 
     return <div> Loading </div>;
   } else {
+    const outerDivStyle = {
+      width: '100%',
+      minHeight: '480px',
+      backgroundColor: 'black',
+      height: 'calc(100vh - 64px)',
+      maxHeight: 'calc((9 / 16) * 100vw',
+    };
+
     return (
       <div>
-        <div
-          style={{
-            width: '100%',
-            height: 'calc(100vh - 64px)',
-            minHeight: '480px',
-            maxHeight: 'calc((9 / 16) * 100vw',
-          }}>
-          <ReactPlayer
-            height='100%'
-            width='100%'
-            controls={true}
-            className='react-player'
-            url={[
-              {
-                src: state.url,
-                type: 'video/mp4',
-              },
-            ]}
-          />
+        <div style={outerDivStyle}>
+          <ReactPlayer height='100%' width='100%' controls={true} url={state.url} />
         </div>
         <div style={{ padding: '10px' }}>
-          {/* <img src='https://s3.us-east-2.wasabisys.com/media-bken/123/thumb.jpg' width='100%' /> */}
           <h3 style={{ color: 'white', padding: '5px' }}>{state.title}</h3>
         </div>
       </div>
