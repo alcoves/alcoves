5b14deab03dceecc64196e799cbcec926e0838f5
Brendan Kennedy <brenwken@gmail.com>
2019-12-19 14:24:14 -0500

    use basename in browser router

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const Index = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   );
