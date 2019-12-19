5f5a58c8a2d9b1272f929c8dd311b88c7f72a81a
Brendan Kennedy <brenwken@gmail.com>
2019-12-19 16:57:27 -0500

    Use http api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl =
-  process.env.NODE_ENV === 'production' ? 'https://api.bken.io' : 'http://localhost:3000';
+  process.env.NODE_ENV === 'production' ? 'http://api.bken.io' : 'http://localhost:3000';
 
 export default config => {
   return new Promise((resolve, reject) => {
