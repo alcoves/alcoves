651844f0313aa1ca2af9ac8fee9de03089cd7e44
Brendan Kennedy <brenwken@gmail.com>
2019-12-23 23:52:19 -0500

    Creating channel work

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserStore from '../../data/User';
+import api from '../../api/api';
 
 import { Spin, Icon } from 'antd';
 import { observer, useObservable } from 'mobx-react-lite';
 
+const loadVideos = async userId => {
+  if (userId) {
+    const { data } = await api({
+      method: 'get',
+      url: `/videos`,
+    });
+
+    console.log('data', data.payload);
+  }
+};
+
 export default observer(() => {
+  const user = useContext(UserStore);
   const state = useObservable({
     loading: false,
     videos: [],
+  });
+
+  loadVideos(user.id).then(() => {
+    console.log('done');
   });
 
   if (state.loading) {
