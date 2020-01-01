744bf8842c40d584f13f2028ff5bc9a6d6b56a5e
Brendan Kennedy <brenwken@gmail.com>
2020-01-01 10:45:15 -0500

    Add default thumnail and date to all video entries

--- pier/api/models/video.js
+++ pier/api/models/video.js
@@ -7,7 +7,12 @@ const mediaSchema = new Schema({
   '1080p': { type: String, required: false },
   '1440p': { type: String, required: false },
   '2160p': { type: String, required: false },
-  thumbnail: { type: String, required: false },
+  thumbnail: {
+    type: String,
+    default:
+      'https://s3.us-east-2.wasabisys.com/media-bken/files/default-thumbnail-sm.jpg',
+    required: false,
+  },
 });
 
 const videoSchema = new Schema({
@@ -16,6 +21,8 @@ const videoSchema = new Schema({
   status: { type: String, required: true },
   media: { type: mediaSchema, required: true },
   sourceFileName: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, required: true },
+  modifiedAt: { type: Date, default: Date.now, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 });
 
