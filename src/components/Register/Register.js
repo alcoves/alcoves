1be81eaca52652c24c4f5ca059031c90b4490983
Brendan Kennedy <brenwken@gmail.com>
2020-01-03 00:23:43 -0500

    Login/Register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,11 +2,14 @@ import React, { useContext } from 'react';
 import User from '../../data/User';
 import api from '../../api/api';
 
-import { Button } from 'semantic-ui-react';
+import { useHistory } from 'react-router-dom';
 import { observer, useObservable } from 'mobx-react-lite';
+import { Button, Form, Grid, Segment } from 'semantic-ui-react';
 
 export default observer(props => {
   const user = useContext(User);
+  const history = useHistory();
+
   const state = useObservable({
     email: '',
     password: '',
@@ -14,72 +17,82 @@ export default observer(props => {
     loading: false,
   });
 
-  const handleTextField = e => {
-    state[e.target.id] = e.target.value;
+  const handleChange = (e, { name, value }) => {
+    state[name] = value;
   };
 
-  const handleLogin = () => {
-    state.loading = true;
-    api({
-      method: 'post',
-      url: '/register',
-      data: {
-        email: state.email,
-        password: state.password,
-        userName: state.userName,
-      },
-    })
-      .then(({ data }) => {
-        state.loading = false;
-        user.login(data.accessToken);
-        props.history.push('/profile');
-      })
-      .catch(error => {
-        console.log(error);
-        state.loading = false;
+  const handleSubmit = async () => {
+    try {
+      state.loading = true;
+      const { data } = await api({
+        method: 'post',
+        url: '/register',
+        data: {
+          email: state.email,
+          password: state.password,
+          userName: state.userName,
+        },
       });
+
+      state.loading = false;
+      user.login(data.accessToken);
+      props.history.push('/');
+    } catch (error) {
+      console.log(error);
+      state.loading = false;
+    }
   };
 
   return (
-    <div>
-      <div
-        xs={22}
-        md={12}
-        lg={8}
-        xxl={5}
-        style={{ margin: '50px 10px 10px 10px', maxWidth: '400px' }}>
-        <input
-          id='userName'
-          size='large'
-          placeholder='username'
-          value={state.userName}
-          onChange={handleTextField}
-        />
-      </div>
-      <div xs={22} md={12} lg={8} xxl={5} style={{ margin: '10px', maxWidth: '400px' }}>
-        <input
-          id='email'
-          size='large'
-          placeholder='email address'
-          value={state.email}
-          onChange={handleTextField}
-        />
-      </div>
-      <div xs={22} md={12} lg={8} xxl={5} style={{ margin: '10px', maxWidth: '400px' }}>
-        <input
-          size='large'
-          id='password'
-          type='password'
-          placeholder='password'
-          value={state.password}
-          onChange={handleTextField}
-        />
-      </div>
-      <div xs={22} md={12} lg={8} xxl={5} style={{ margin: '10px', maxWidth: '400px' }}>
-        <Button block onClick={handleLogin} disabled={state.loading}>
-          Register
-        </Button>
-      </div>
-    </div>
+    <Grid textAlign='center' style={{ marginTop: '50px' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Form size='large' onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon='user'
+              name='userName'
+              iconPosition='left'
+              value={state.userName}
+              placeholder='Username'
+              onChange={handleChange}
+            />
+            <Form.Input
+              fluid
+              icon='mail'
+              name='email'
+              iconPosition='left'
+              value={state.email}
+              placeholder='E-mail address'
+              onChange={handleChange}
+            />
+            <Form.Input
+              fluid
+              icon='lock'
+              type='password'
+              name='password'
+              iconPosition='left'
+              placeholder='Password'
+              value={state.password}
+              onChange={handleChange}
+            />
+            <Grid>
+              <Grid.Column width={10}>
+                <Form.Button color='teal' fluid content='Register' />
+              </Grid.Column>
+              <Grid.Column width={6}>
+                <Button
+                  color='teal'
+                  basic
+                  fluid
+                  content='Or Login'
+                  onClick={() => history.push('/login')}
+                />
+              </Grid.Column>
+            </Grid>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 });
