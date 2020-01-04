e84593a352e2f94ae7d1c34234c4c26f4630a8fb
Brendan Kennedy <brenwken@gmail.com>
2020-01-04 04:16:13 -0500

    Support new file formats

--- src/components/VideoEditor/VideoEditor.js
+++ src/components/VideoEditor/VideoEditor.js
@@ -1,9 +1,47 @@
-import React from 'react';
 import api from '../../api/api';
+import React, { useState, useEffect, useRef } from 'react';
 
-import { Button } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom';
 import { observer, useObservable } from 'mobx-react-lite';
+import { Button, Label, Icon, Container } from 'semantic-ui-react';
+
+function timeConversion(startTime, completeTime) {
+  const millisec = new Date(completeTime).getMilliseconds() - new Date(startTime).getMilliseconds();
+  const seconds = (millisec / 1000).toFixed(1);
+  const minutes = (millisec / (1000 * 60)).toFixed(1);
+  const hours = (millisec / (1000 * 60 * 60)).toFixed(1);
+  const days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
+
+  if (seconds < 60) {
+    return seconds + ' Sec';
+  } else if (minutes < 60) {
+    return minutes + ' Min';
+  } else if (hours < 24) {
+    return hours + ' Hrs';
+  } else {
+    return days + ' Days';
+  }
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 export default observer(props => {
   const history = useHistory();
@@ -11,6 +49,8 @@ export default observer(props => {
     video: {},
     loading: true,
   });
+
+  let [count, setCount] = useState(0);
 
   const handleRefresh = async () => {
     try {
@@ -26,6 +66,18 @@ export default observer(props => {
       state.loading = false;
     }
   };
+
+  if (state.loading === true) {
+    handleRefresh();
+  }
+
+  useInterval(() => {
+    if (state.video.status !== 'completed') {
+      handleRefresh();
+    }
+
+    setCount(count + 1);
+  }, 3000);
 
   const handleDelete = async e => {
     try {
@@ -44,9 +96,31 @@ export default observer(props => {
     history.push(`/videos/${e.target.id}`);
   };
 
-  if (state.loading) {
-    handleRefresh();
+  const renderFiles = () => {
+    return Object.entries(state.video.files).map(([quality, fileObj]) => {
+      return (
+        <div key={quality} style={{ margin: '5px 0px 5px 0px' }}>
+          <Label as='a' color='grey'>
+            {fileObj.status === 'completed' ? (
+              <Icon color='green' name='check circle outline' />
+            ) : (
+              <Icon color='yellow' loading name='setting' />
+            )}
+            {quality}
+            <Label.Detail>{`${fileObj.percentCompleted}%`}</Label.Detail>
+            {fileObj.conversionStartTime && fileObj.conversionCompleteTime ? (
+              <Label.Detail>{`took ${timeConversion(
+                fileObj.conversionStartTime,
+                fileObj.conversionCompleteTime,
+              )}`}</Label.Detail>
+            ) : null}
+          </Label>
+        </div>
+      );
+    });
+  };
 
+  if (state.loading) {
     return (
       <div>
         <h1>loading</h1>
@@ -54,38 +128,20 @@ export default observer(props => {
     );
   } else {
     return (
-      <div>
-        <Button type='default' onClick={handleRefresh}>
-          Refresh
-        </Button>
-        <div
-          style={{
-            width: '400px',
-            height: 'auto',
-            backgroundColor: 'rgb(0, 21, 41)',
-            border: 'solid white 1px',
-            margin: '10px',
-            padding: '10px',
-          }}>
-          <h1>{state.video.title}</h1>
-          <h5>Video ID : {state.video._id}</h5>
-          <h5>Authord ID : {state.video.author}</h5>
-          <p>
-            Status:
-            {state.video.status}
-          </p>
-          {state.video.media &&
-            Object.entries(state.video.media).map(([k, v]) => {
-              return <p key={k}>{k}</p>;
-            })}
-          <Button id={state.video._id} type='primary' onClick={handleView}>
-            View
-          </Button>
-          <Button id={state.video._id} type='danger' onClick={handleDelete}>
-            Delete
-          </Button>
+      <Container>
+        <h1>{state.video.title}</h1>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <div>{renderFiles()}</div>
+          <div>
+            <Button id={state.video._id} type='primary' onClick={handleView}>
+              View
+            </Button>
+            <Button id={state.video._id} type='danger' onClick={handleDelete}>
+              Delete
+            </Button>
+          </div>
         </div>
-      </div>
+      </Container>
     );
   }
 });
