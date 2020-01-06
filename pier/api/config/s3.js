d81c0ed7e6f363e7ebd0d1a2f6b00289b17b2e9f
Brendan Kennedy <brenwken@gmail.com>
2020-01-05 23:12:05 -0500

    Add aws sdk http retries

--- pier/api/config/s3.js
+++ pier/api/config/s3.js
@@ -5,6 +5,11 @@ const { WASABI_ENDPOINT } = require('./config');
 AWS.config.update({
   accessKeyId: process.env.WASABI_ACCESS_KEY_ID,
   secretAccessKey: process.env.WASABI_SECRET_ACCESS_KEY,
+  maxRetries: 2,
+  httpOptions: {
+    timeout: 5000,
+    connectTimeout: 3000,
+  },
 });
 
 const s3 = new AWS.S3({
