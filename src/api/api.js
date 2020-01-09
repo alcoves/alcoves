57cf1cda13653ae7291390e653450cf05749b6ec
Brendan Kennedy <brenwken@gmail.com>
2020-01-09 18:42:28 -0500

    async api handler

--- src/api/api.js
+++ src/api/api.js
@@ -3,21 +3,13 @@ import axios from 'axios';
 const baseUrl =
   process.env.NODE_ENV === 'production' ? 'https://api.bken.io' : 'http://localhost:3000';
 
-export default config => {
-  return new Promise((resolve, reject) => {
+export default async config => {
+  try {
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) config.headers = { authorization: `Bearer ${accessToken}` };
     config.url = `${baseUrl}${config.url}`;
     console.log('axios config', JSON.stringify(config, null, 2));
-    axios(config)
-      .then(resolve)
-      .catch(reject);
-  });
-};
-
-export const login = async data => {
-  try {
-    return axios.post(`${baseUrl}/users/login`, data);
+    return axios(config);
   } catch (error) {
     console.error(error);
     throw error;
