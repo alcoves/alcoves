ace625aa2bb3638299707fd835a39f044121e2a6
Brendan Kennedy <brenwken@gmail.com>
2020-01-12 16:13:41 -0500

    User profile work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,47 +6,6 @@ import { useHistory } from 'react-router-dom';
 import { Loader, Button, Grid } from 'semantic-ui-react';
 import { observer, useObservable } from 'mobx-react-lite';
 
-const styles = {
-  card: {
-    // minWidth: '320px',
-    // width: '100%',
-    maxWidth: '500px',
-    padding: 0,
-    margin: '10px',
-    borderRadius: '5px',
-    backgroundColor: '#272d3c',
-    color: 'white',
-    overflow: 'hidden',
-    WebkitBoxShadow: '3px 8px 42px -8px rgba(0,0,0,0.48)',
-    MozBoxShadow: '3px 8px 42px -8px rgba(0,0,0,0.48)',
-    boxShadow: '3px 8px 42px -8px rgba(0,0,0,0.48)',
-  },
-  meta: {
-    height: '100px',
-    padding: '10px',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-  },
-  title: {
-    fontSize: '1.1em',
-    cursor: 'pointer',
-    height: '50px',
-  },
-  image: {
-    objectFit: 'cover',
-    minHeight: '180px',
-    maxHeight: '180px',
-    width: '100%',
-    cursor: 'pointer',
-  },
-  cardFooter: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-};
-
 export default observer(() => {
   const user = useContext(UserStore);
   const history = useHistory();
@@ -55,9 +14,7 @@ export default observer(() => {
     loading: true,
   });
 
-  if (!user.isLoggedIn()) {
-    return <p> You must login to view this page </p>;
-  } else if (state.loading) {
+  if (state.loading) {
     api({
       method: 'get',
       url: `/videos`,
@@ -67,38 +24,7 @@ export default observer(() => {
     });
 
     return <Loader active inline='centered' style={{ marginTop: '30px' }} />;
-  } else if (state.videos.length) {
-    return (
-      <Grid centered doubling style={{ paddingTop: '30px' }}>
-        {state.videos.map(video => {
-          return (
-            <Grid.Column key={video._id} mobile={14} tablet={10} computer={4} style={styles.card}>
-              <img
-                style={styles.image}
-                alt='thumbnail'
-                src={video.thumbnail}
-                onClick={() => history.push(`/videos/${video._id}`)}></img>
-              <div style={styles.meta}>
-                <div onClick={() => history.push(`/videos/${video._id}`)} style={styles.title}>
-                  {video.title}
-                </div>
-                <div style={styles.cardFooter}>
-                  <Button.Group size='mini' color='teal' basic>
-                    <Button
-                      icon='setting'
-                      onClick={() => {
-                        history.push(`/editor/videos/${video._id}`);
-                      }}
-                    />
-                  </Button.Group>
-                </div>
-              </div>
-            </Grid.Column>
-          );
-        })}
-      </Grid>
-    );
   } else {
-    return <h1>You don't have any videos</h1>;
+    return <div> We are working on a new homepage! </div>;
   }
 });
