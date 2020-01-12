6a6e4fcd68ee8e0055640eadc1422071d5072429
Brendan Kennedy <brenwken@gmail.com>
2020-01-12 15:28:52 -0500

    Get users videos by userId

--- pier/api/controllers/users.js
+++ pier/api/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const Video = require('../models/video');
 
 exports.getUserById = async (req, res) => {
   try {
@@ -7,6 +8,18 @@ exports.getUserById = async (req, res) => {
       payload: await User.findOne({ _id: req.params.userId }).select(
         '-password -email'
       ),
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ message: 'bad request' });
+  }
+};
+
+exports.getUserVideosByUserId = async (req, res) => {
+  try {
+    res.status(200).send({
+      message: 'successfully fetched user videos',
+      payload: await Video.find({ author: req.params.userId }),
     });
   } catch (error) {
     console.error(error);
--- pier/api/routes/users.js
+++ pier/api/routes/users.js
@@ -4,5 +4,6 @@ const c = require('../controllers/users');
 const router = express.Router();
 
 router.get('/:userId', c.getUserById);
+router.get('/:userId/videos', c.getUserVideosByUserId);
 
 module.exports = router;
