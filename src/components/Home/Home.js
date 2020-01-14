19af00e8839d0890a2befe6f33acf81107746db1
Brendan Kennedy <brenwken@gmail.com>
2020-01-13 23:58:35 -0500

    Homepage work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,67 @@
+import './Home.css';
+
 import api from '../../api/api';
 import UserStore from '../../data/User';
 import React, { useContext } from 'react';
 
+import { Loader } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom';
-import { Loader, Button, Grid } from 'semantic-ui-react';
 import { observer, useObservable } from 'mobx-react-lite';
+
+const SidebarNav = observer(() => {
+  const history = useHistory();
+  const state = useObservable({
+    followings: [],
+    loading: true,
+  });
+
+  const loadFollowings = async () => {
+    try {
+      api({
+        method: 'get',
+        url: `/followings`,
+      }).then(res => {
+        console.log(res.data.payload);
+        state.followings = res.data.payload;
+        state.loading = false;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const styles = {
+    container: {
+      backgroundColor: '#171B24',
+      width: '200px',
+      height: 'calc(100vh - 50px)',
+    },
+  };
+
+  if (state.loading) {
+    loadFollowings();
+    return <div style={styles.container}> loading </div>;
+  } else {
+    return (
+      <div style={styles.container}>
+        <h4>Following</h4>
+        {state.followings.map(following => {
+          console.log('following', following.followeeId.userName);
+          return (
+            <div
+              onClick={() => {
+                history.push(`/users/${following.followeeId._id}`);
+              }}
+              key={following._id}
+              className='followerMenuItem'>
+              <p>{following.followeeId.userName}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+});
 
 export default observer(() => {
   const user = useContext(UserStore);
@@ -25,6 +82,11 @@ export default observer(() => {
 
     return <Loader active inline='centered' style={{ marginTop: '30px' }} />;
   } else {
-    return <div> We are working on a new homepage! </div>;
+    return (
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <SidebarNav />
+        <div>We are working on a new homepage!</div>{' '}
+      </div>
+    );
   }
 });
