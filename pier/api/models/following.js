f51336ec89b71c2faede81b7c56ff808d93c6297
Brendan Kennedy <brenwken@gmail.com>
2020-01-14 00:55:38 -0500

    Change database table name

--- pier/api/models/following.js
+++ pier/api/models/following.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const followerSchema = new Schema(
+const followings = new Schema(
   {
     _id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId() },
     followerId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
@@ -10,6 +10,6 @@ const followerSchema = new Schema(
   { timestamps: true }
 );
 
-followerSchema.index({ followerId: 1, followeeId: 1 }, { unique: true });
+followings.index({ followerId: 1, followeeId: 1 }, { unique: true });
 
-module.exports = mongoose.model('Follower', followerSchema);
+module.exports = mongoose.model('Followings', followings);
