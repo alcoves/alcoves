48213e32479555aa18f84080157cf4b092156fb6
Brendan Kennedy <brenwken@gmail.com>
2020-01-13 21:41:24 -0500

    Cleanup user export

--- pier/api/models/user.js
+++ pier/api/models/user.js
@@ -16,6 +16,4 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
