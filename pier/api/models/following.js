6d84cf577e2a75797c39b0be88011e4e738889d7
Brendan Kennedy <brenwken@gmail.com>
2020-01-14 01:00:31 -0500

    Following indexes

--- pier/api/models/following.js
+++ pier/api/models/following.js
@@ -4,12 +4,22 @@ const Schema = mongoose.Schema;
 const followings = new Schema(
   {
     _id: { type: Schema.Types.ObjectId, default: mongoose.Types.ObjectId() },
-    followerId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    followeeId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    followerId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
+    followeeId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true,
+    },
   },
   { timestamps: true }
 );
 
-followings.index({ followerId: 1, followeeId: 1 }, { unique: true });
+// followings.index({ followerId: 1, followeeId: 1 }, { unique: true });
 
 module.exports = mongoose.model('Followings', followings);
