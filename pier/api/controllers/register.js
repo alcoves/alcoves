abc2bd33532715241664396a8647f400f9a44f80
Brendan Kennedy <brenwken@gmail.com>
2020-01-19 22:10:11 -0500

    Jest tests

--- pier/api/controllers/register.js
+++ pier/api/controllers/register.js
@@ -4,38 +4,34 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 exports.register = async (req, res) => {
-  try {
-    const userExists = await User.find({ email: req.body.email });
+  const userExists = await User.find({ email: req.body.email });
 
-    if (userExists.length >= 1) {
-      return res.status(409).send({
-        message: 'user already exists',
-      });
-    }
-
-    if (req.body.code !== process.env.BETA_CODE) {
-      return res.status(400).send({
-        message: 'bad beta code',
-      });
-    }
-
-    const user = new User({
-      _id: mongoose.Types.ObjectId(),
-      email: req.body.email,
-      password: await bcrypt.hash(req.body.password, 10),
-      displayName: req.body.displayName,
+  if (userExists.length >= 1) {
+    return res.status(409).send({
+      message: 'user already exists',
     });
-
-    await user.save();
-
-    const accessToken = jwt.sign(
-      { email: user.email, id: user.id, displayName: user.displayName },
-      process.env.JWT_KEY,
-      { expiresIn: '7d' }
-    );
-
-    res.status(201).send({ message: 'registration successful', accessToken });
-  } catch (error) {
-    throw error;
   }
+
+  if (req.body.code !== process.env.BETA_CODE) {
+    return res.status(400).send({
+      message: 'bad beta code',
+    });
+  }
+
+  const user = await User({
+    _id: mongoose.Types.ObjectId(),
+    email: req.body.email,
+    password: await bcrypt.hash(req.body.password, 10),
+    displayName: req.body.displayName,
+  }).save();
+
+  const accessToken = jwt.sign(
+    { email: user.email, id: user.id, displayName: user.displayName },
+    process.env.JWT_KEY,
+    { expiresIn: '7d' }
+  );
+
+  res
+    .status(201)
+    .send({ message: 'registration successful', accessToken, _id: user._id });
 };
--- pier/jest.config.js
+++ pier/jest.config.js
@@ -1,1 +1,5 @@
-module.exports = { testEnvironment: 'node' };
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverage: true,
+  silent: true,
+};
