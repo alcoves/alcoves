8659ae9f9c5d4c2b92938195b866626b1a83fc45
Brendan Kennedy <brenwken@gmail.com>
2020-01-23 16:56:05 -0500

    Start on rest api replacement with apollo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,34 @@
 import './Home.css';
 
-import api from '../../api/api';
+import gql from 'graphql-tag';
 import React from 'react';
 
 import { Loader } from 'semantic-ui-react';
 import { observer, useObservable } from 'mobx-react-lite';
+import { useQuery } from '@apollo/react-hooks';
+import VideoGrid from '../VideoGrid/VideoGrid';
+
+const GET_VIDEOS = gql`
+  {
+    videos {
+      id
+      title
+      views
+      thumbnail
+    }
+  }
+`;
 
 export default observer(() => {
-  const state = useObservable({
-    videos: [],
-    loading: true,
-  });
+  const { loading, data } = useQuery(GET_VIDEOS);
 
-  if (state.loading) {
-    api({
-      method: 'get',
-      url: `/videos`,
-    }).then(res => {
-      state.videos = res.data.payload;
-      state.loading = false;
-    });
-
+  if (loading) {
     return <Loader active inline='centered' style={{ marginTop: '30px' }} />;
-  } else {
+  } else if (data) {
     return (
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <div>We are working on a new homepage!</div>{' '}
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div>We are working on a new homepage!</div>
+        <VideoGrid videos={data.videos} />
       </div>
     );
   }
