d7024fcb229e978bd2416aa241a4e2717f16a847
Brendan Kennedy <brenwken@gmail.com>
2020-01-27 13:42:55 -0500

    Make videos load again

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,132 +1,166 @@
 import dayjs from 'dayjs';
 import React from 'react';
+import gql from 'graphql-tag';
 import api from '../../api/api';
 import useInterval from '../../lib/useInterval';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 import { useHistory } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
 import { Loader, Container } from 'semantic-ui-react';
 import { observer, useObservable } from 'mobx-react-lite';
 
 dayjs.extend(relativeTime);
 
-const pickVideoUrl = files => {
-  if (files['2160p'] && files['2160p'].link) return { format: '2160p', link: files['2160p'].link };
-  if (files['1440p'] && files['1440p'].link) return { format: '1440p', link: files['1440p'].link };
-  if (files['1080p'] && files['1080p'].link) return { format: '1080p', link: files['1080p'].link };
-  if (files['720p'] && files['720p'].link) return { format: '720p', link: files['720p'].link };
-  if (files['highQuality'] && files['highQuality'].link)
-    return { format: 'highQuality', link: files['highQuality'].link };
-};
-
-export default observer(props => {
-  const history = useHistory();
-  const state = useObservable({
-    video: {},
-    url: '',
-    loading: true,
-  });
-
-  const handleRefresh = () => {
-    api({ url: `/videos/${props.id}`, method: 'get' }).then(({ data }) => {
-      const quality = pickVideoUrl(data.payload.files);
-
-      if (!quality) {
-        state.loading = true;
-      } else {
-        state.video = data.payload;
-        state.url = quality.link;
-        state.loading = false;
+const pickUrl = files => {
+  let url;
+  const loadOrder = ['2160p', '1440p', '1080p', '720p', 'highQuality'];
+  loadOrder.map(desiredPreset => {
+    files.map(({ preset, link }) => {
+      // console.log(desiredPreset, preset, link);
+      if (desiredPreset === preset && link) {
+        url = link;
       }
     });
-  };
+  });
 
-  if (state.loading === true) {
-    handleRefresh();
+  return url;
+};
+
+export default props => {
+  const GET_VIDEO = gql`
+  {
+    video(id: "${props.id}") {
+      id
+      title
+      views
+      status
+      user {
+        id
+        avatar
+        displayName
+      }
+      files {
+        link
+        status
+        preset
+      }
+    }
   }
+`;
 
-  useInterval(() => {
-    if (state.loading) handleRefresh();
-  }, 3000);
+  const { loading, data } = useQuery(GET_VIDEO);
 
-  if (state.loading || !state.url) {
+  if (loading) {
     return <Loader active inline='centered' style={{ marginTop: '30px' }} />;
-  } else {
+  } else if (data) {
     const outerDivStyle = {
       backgroundColor: '#000000',
       height: 'calc(100vh - 100px)',
       maxHeight: 'calc((9 / 16) * 100vw',
     };
 
-    console.log(JSON.stringify(state.video));
+    const link = pickUrl(data.video.files);
+    console.log(link);
 
     return (
-      <div>
-        <div style={outerDivStyle}>
-          <video width='100%' height='100%' controls autoPlay>
-            <source src={state.url} type='video/mp4' />
-          </video>
-        </div>
-
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
         <div>
-          <Container style={{ marginTop: '20px' }}>
-            <div>
-              <h2>{state.video.title}</h2>
-              <p>
-                {state.video.views} views • {dayjs(state.video.createdAt).fromNow()} •
-                {state.url.split('/')[state.url.split('/').length - 1].split('.')[0]}
-              </p>
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                marginTop: '10px',
-                height: '75px',
-              }}>
+          <div style={outerDivStyle}>
+            <video width='100%' height='100%' controls autoPlay>
+              <source src={link} type='video/mp4' />
+            </video>
+          </div>
+          <div>
+            <Container style={{ marginTop: '20px' }}>
+              <div>
+                <h2>{data.video.title}</h2>
+                <p>
+                  {data.video.views} views • {dayjs(data.video.createdAt).fromNow()}
+                </p>
+              </div>
               <div
                 style={{
                   display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  marginRight: '10px',
+                  marginTop: '10px',
+                  height: '75px',
                 }}>
-                <img
-                  width={50}
-                  height={50}
-                  alt='profile'
-                  src={state.video.user.avatar}
-                  style={{
-                    borderRadius: '50%',
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => history.push(`/users/${state.video.user._id}`)}
-                />
-              </div>
-
-              <div style={{ height: '100%' }}>
                 <div
                   style={{
                     display: 'flex',
-                    alignItems: 'flex-end',
-                    height: '50%',
-                    // border: 'blue solid 1px',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginRight: '10px',
                   }}>
-                  {state.video.user.displayName}
+                  <img
+                    width={50}
+                    height={50}
+                    alt='profile'
+                    src={data.video.user.avatar}
+                    style={{
+                      borderRadius: '50%',
+                      cursor: 'pointer',
+                    }}
+                    onClick={() => history.push(`/users/${data.video.user._id}`)}
+                  />
                 </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    alignItems: 'flex-start',
-                    height: '50%',
-                    // border: 'blue solid 1px',
-                  }}>
-                  {state.video.user.followers} followers
+                <div style={{ height: '100%' }}>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      height: '50%',
+                      // border: 'blue solid 1px',
+                    }}>
+                    {data.video.user.displayName}
+                  </div>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'flex-start',
+                      height: '50%',
+                      // border: 'blue solid 1px',
+                    }}>
+                    {data.video.user.followers} followers
+                  </div>
                 </div>
               </div>
-            </div>
-          </Container>
+            </Container>
+          </div>
         </div>
       </div>
     );
   }
-});
+};
+
+// export default observer(props => {
+//   const history = useHistory();
+//   const data.video = useObservable({
+//     video: {},
+//     url: '',
+//     loading: true,
+//   });
+
+//   const handleRefresh = () => {
+//     api({ url: `/videos/${props.id}`, method: 'get' }).then(({ data }) => {
+//       const quality = pickVideoUrl(data.payload.files);
+
+//       if (!quality) {
+//         data.video.loading = true;
+//       } else {
+//         data.video.video = data.payload;
+//         videoUrl = quality.link;
+//         data.video.loading = false;
+//       }
+//     });
+//   };
+
+//   if (data.video.loading === true) {
+//     handleRefresh();
+//   }
+
+//   useInterval(() => {
+//     if (data.video.loading) handleRefresh();
+//   }, 3000);
+
+// });
