afdf2729e3b6181558980961ccf309116dbd8f72
Brendan Kennedy <brenwken@gmail.com>
2020-02-05 19:33:27 -0500

    Improve uploader re-rendering performance

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,18 +1,18 @@
+import './Upload.css';
 import axios from 'axios';
-import React, { useState } from 'react';
+import chunkFile from '../../utils/chunkFile';
+import React, { useState, useEffect } from 'react';
 
+import { useHistory } from 'react-router-dom';
 import { useMutation } from '@apollo/react-hooks';
 import { Button, Progress, Loader } from 'semantic-ui-react';
 import { createMultipartUploadMutation, completedMultipartUploadMutation } from '../../lib/queries';
 
-import chunkFile from '../../utils/chunkFile';
-
 export default () => {
-  const [state, setState] = useState({
-    fileList: [],
-    uploading: false,
-    bytesUploaded: 0,
-  });
+  const history = useHistory();
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+  let [bytesUploaded, setBytesUploaded] = useState(0);
 
   const [
     startUpload,
@@ -24,17 +24,17 @@ export default () => {
     { called: comCalled, loading: comLoading, data: comData, error: comError },
   ] = useMutation(completedMultipartUploadMutation);
 
-  if (creData && !state.uploading && !comCalled) {
-    setState({ uploading: true });
+  if (creData && !uploading && !comCalled) {
+    setUploading(true);
     Promise.all(
-      chunkFile(state.fileList[0]).reduce((acc, blob, partIndex) => {
+      chunkFile(files[0]).reduce((acc, blob, partIndex) => {
         let lastBytesUploaded = 0;
         console.log('getting called to upload');
         acc.push(
           axios.put(creData.createMultipartUpload.urls[partIndex], blob, {
-            headers: { 'Content-Type': state.fileList[0].type },
+            headers: { 'Content-Type': files[0].type },
             onUploadProgress: e => {
-              state.bytesUploaded += e.loaded - lastBytesUploaded;
+              setBytesUploaded((bytesUploaded += e.loaded - lastBytesUploaded));
               lastBytesUploaded = e.loaded;
             },
           }),
@@ -49,91 +49,97 @@ export default () => {
       }, []);
 
       const { objectId, key, uploadId } = creData.createMultipartUpload;
+      console.log('--- COMPLETING VIDEO UPLOAD ---');
       completeUpload({ variables: { input: { objectId, parts, key, uploadId } } });
     });
   }
 
-  if (creCalled) console.log('creCalled');
-  if (creLoading) console.log('creLoading');
-  if (comCalled) console.log('creCalled');
+  console.log({
+    creCalled,
+    creLoading,
+    creData,
+    creError,
+    comCalled,
+    comLoading,
+    comData,
+    comError,
+  });
 
-  if (creError) {
-    setState({ fileList: [], uploading: false, bytesUploaded: 0 });
+  if (creError || comError) {
+    setUploading(false);
+    setBytesUploaded(0);
+    setFiles([]);
     console.log('creError', creError);
-  }
-
-  if (comError) {
-    setState({ fileList: [], uploading: false, bytesUploaded: 0 });
     console.log('comError', comError);
   }
 
-  if (comData && comCalled && !comLoading) {
-    setState({ fileList: [], uploading: false, bytesUploaded: 0 });
+  if (comData && comCalled && !comLoading && uploading) {
+    setUploading(false);
+    setBytesUploaded(0);
+    setFiles([]);
     console.log('i should be redirecting!', comData);
   }
+
+  useEffect(() => {
+    if (files.length) {
+      console.log('starting file upload process');
+      const fileType = files[0].type;
+      const parts = chunkFile(files[0]).length;
+      startUpload({ variables: { input: { parts, fileType } } });
+    }
+  }, [files]);
 
   const fileInputRef = React.createRef();
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        padding: '20px 5px 5px 5px',
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-        flexDirection: 'column',
-      }}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexDirection: 'column',
-          width: '300px',
-        }}>
-        <div style={{ margin: '10px 0px 10px 0px' }}>
-          <Button
-            content='Select Video'
-            labelPosition='left'
-            icon='video'
-            fluid
-            disabled={comLoading || creLoading || state.uploading}
-            onClick={() => fileInputRef.current.click()}
-          />
-        </div>
+    <div className='uploadContainer'>
+      <div className='uploadRow'>
+        <Button
+          fluid
+          icon='video'
+          labelPosition='left'
+          content='Select Video'
+          onClick={() => fileInputRef.current.click()}
+          disabled={comLoading || creLoading || uploading}
+        />
         <input
-          ref={fileInputRef}
+          hidden
           type='file'
           name='video'
-          accept='video/mp4'
-          files={state.fileList}
           type='file'
-          hidden
+          accept='video/mp4'
+          ref={fileInputRef}
           onChange={e => {
-            if (!state.fileList.length) {
-              setState({ fileList: [e.target.files[0]], uploading: false, bytesUploaded: 0 });
-              const fileType = e.target.files[0].type;
-              const parts = chunkFile(e.target.files[0]).length;
-              startUpload({ variables: { input: { parts, fileType } } });
+            if (!files.length) {
+              setFiles([e.target.files[0]]);
             }
           }}
         />
-        <div style={{ margin: '10px 0px 10px 0px' }}>
-          {state.bytesUploaded && state.fileList[0].size ? (
+        <div className='uploadRow'>
+          {bytesUploaded && files[0].size ? (
             <Progress
               size='small'
-              percent={((state.bytesUploaded / state.fileList[0].size) * 100).toFixed(0)}
               progress='percent'
+              percent={((bytesUploaded / files[0].size) * 100).toFixed(0)}
             />
           ) : null}
           {comLoading && (
-            <div style={{ margin: '10px 0px 10px 0px' }}>
-              <Loader active inline='centered'>
-                Completing upload...
-              </Loader>
-            </div>
+            <Loader active inline='centered'>
+              Completing upload...
+            </Loader>
           )}
+        </div>
+        <div className='uploadRow'>
+          {comData && creData ? (
+            <Button
+              fluid
+              positive
+              onClick={() => {
+                history.push(`/editor/videos/${creData.createMultipartUpload.objectId}`);
+              }}>
+              View In Editor
+            </Button>
+          ) : null}
         </div>
       </div>
     </div>
