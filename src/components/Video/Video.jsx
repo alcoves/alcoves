1d2be295233e145b1382d7c71504f1f4e6668456
Brendan Kennedy <brenwken@gmail.com>
2020-02-05 20:39:45 -0500

    Remove link log

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -53,7 +53,6 @@ export default props => {
     };
 
     const link = pickUrl(data.video.files);
-    console.log(link);
 
     return (
       <div style={{ display: 'flex', flexDirection: 'column' }}>
