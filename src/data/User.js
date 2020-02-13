5338b08996386f112f8563f234f0c46a817cc61c
Brendan Kennedy <brenwken@gmail.com>
2020-02-12 22:07:09 -0500

    Cleanup user store

--- src/data/User.js
+++ src/data/User.js
@@ -1,55 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import jwt from 'jsonwebtoken';
-
-// const user = () => {
-//   const [user, setUser] = useState({});
-
-//   const isLoggedIn = () => {
-//     return user.id ? true : false;
-//   };
-
-//   const login = async accessToken => {
-//     try {
-//       if (accessToken) {
-//         const decoded = jwt.decode(accessToken, { complete: true });
-
-//         const isTokenExpired = token => {
-//           return token.payload.exp < Date.now() / 1000;
-//         };
-
-//         if (decoded && !isTokenExpired(decoded)) {
-//           localStorage.setItem('accessToken', accessToken);
-//           setUser({
-//             id: decoded.payload.id,
-//             email: decoded.payload.email,
-//             displayName: decoded.payload.displayName,
-//           });
-//         } else {
-//           logout();
-//         }
-//       }
-//     } catch (error) {
-//       logout();
-//       console.error(error);
-//       throw error;
-//     }
-//   };
-
-//   const logout = async (refresh = false) => {
-//     try {
-//       // TODO :: Intergrate with apollo client client.resetStore()
-//       // https://www.apollographql.com/docs/react/networking/authentication/
-
-//       console.log('Logging user out');
-//       setUser();
-//       await localStorage.removeItem('accessToken');
-//       if (refresh) window.location.reload();
-//     } catch (error) {
-//       console.error(error);
-//       throw error;
-//     }
-//   };
-// };
 
 class User {
   constructor() {
