df68906ccc271f0951ab0f308d1407535cc8ac02
Brendan Kennedy <brenwken@gmail.com>
2020-02-18 01:12:30 -0500

    Add service api auth

--- pier/src/app.js
+++ pier/src/app.js
@@ -11,6 +11,12 @@ const server = new ApolloServer({
   context: ({ req }) => {
     const token = req.headers.authorization || '';
     if (token) {
+      if (token.includes('SERVICE')) {
+        if (!token.split('SERVICE ')[1] === process.env.CONVERSION_API_KEY) {
+          throw new Error('invalid service api key');
+        }
+      }
+
       const user = jwt.verify(token.split(' ')[1], process.env.JWT_KEY);
       return { user };
     }
