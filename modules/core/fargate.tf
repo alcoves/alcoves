42cfa902d85afd5f998f03f8b77bcd914f2a07b9
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 18:17:23 -0400

    Add terraform stack

---
+++ modules/core/fargate.tf
@@ -0,0 +1,65 @@
+data "aws_ecs_cluster" "pier11" {
+  cluster_name = "pier11"
+}
+
+data "template_file" "web_container_def" {
+  template = file("${path.module}/templates/fargate_task.json")
+
+  vars = {
+    credentials = "github_registry_login"
+    log_group   =  aws_cloudwatch_log_group.web.name
+    app_image   = "docker.pkg.github.com/bken-io/web/web:dev"
+  }
+}
+
+resource "aws_ecs_task_definition" "web" {
+  cpu                      = 256
+  memory                   = 512
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  family                   = "web"
+  execution_role_arn       = "arn:aws:iam::594206825329:role/ecsTaskAll"
+  task_role_arn            = "arn:aws:iam::594206825329:role/ecsTaskAll"
+  container_definitions    = data.template_file.web_container_def.rendered
+}
+
+resource "aws_ecs_service" "web" {
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  name            = "${var.env}-web"
+  depends_on      = [aws_alb_target_group.web]
+  cluster         = data.aws_ecs_cluster.pier11.id
+  task_definition = aws_ecs_task_definition.web.arn
+
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.web_security_group.id]
+    subnets          = [
+      data.aws_subnet.sub1.id, 
+      data.aws_subnet.sub2.id,
+      data.aws_subnet.sub3.id,
+      data.aws_subnet.sub4.id,
+      data.aws_subnet.sub5.id,
+      data.aws_subnet.sub6.id,
+    ]
+  }
+
+  load_balancer {
+    container_port   = 80
+    container_name   = "web"
+    target_group_arn = aws_alb_target_group.web.arn
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_log_group" "web" {
+  name = "services/${var.env}/web"
+
+  tags = {
+    Environment = var.env
+    Application = "web"
+  }
+}
---
+++ modules/core/networking.tf
@@ -0,0 +1,35 @@
+data "aws_lb" "services" {
+  name = "${var.env}-services"
+}
+
+resource "aws_security_group" "web_security_group" {
+  name        = "web"
+  description = "Allows web traffic"
+  vpc_id      = data.aws_vpc.bken1.id
+}
+
+resource "aws_security_group_rule" "web_ingress" {
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_security_group.id
+}
+
+resource "aws_security_group_rule" "web_egress" {
+  from_port         = 1
+  to_port           = 65535
+  protocol          = "tcp"
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_security_group.id
+}
+
+resource "aws_alb_target_group" "web" {
+  port        = 80
+  target_type = "ip"
+  name        = "web"
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.bken1.id
+}
