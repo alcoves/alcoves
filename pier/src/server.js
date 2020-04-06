8e17ea6d4dd5eca32574f9558dea6a31bfcf8d72
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 22:51:27 -0400

    Re add health check at root

--- pier/src/server.js
+++ pier/src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 4000;
 
+app.get('/', (req, res) => res.send('taffy3'))
 app.get('/api', (req, res) => res.send('taffy3'))
 
 app.listen({ port }, () =>
