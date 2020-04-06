48acd36232343694e721db88eab55acad56f6039
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 21:54:07 -0400

    Add api health check

--- pier/src/server.js
+++ pier/src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 4000;
 
+app.get('/', () => res.send('taffy3'))
+
 app.listen({ port }, () =>
   console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
 );
