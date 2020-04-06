900e9eec73704f683ce9b2dd2ec8a1b2aedfc12b
Brendan Kennedy <brenwken@gmail.com>
2020-04-05 22:01:27 -0400

    Fix health check

--- pier/src/server.js
+++ pier/src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 4000;
 
-app.get('/', () => res.send('taffy3'))
+app.get('/', (req, res) => res.send('taffy3'))
 
 app.listen({ port }, () =>
   console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
