fd15859d6137f1b09de792e2260ec7fb5731f8e5
Brendan Kennedy <brenwken@gmail.com>
2020-04-15 23:20:28 -0400

    Cleanup originUrl

--- pier/src/app.js
+++ pier/src/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
-const {
-  NODE_ENV,
-  BKEN_ENV
-} = process.env
+const { NODE_ENV, BKEN_ENV } = process.env;
 
-const getOrigin = () => {
-  if (BKEN_ENV === 'prod' && NODE_ENV === 'production') return 'https://bken.io'
-  if (BKEN_ENV === 'dev' && NODE_ENV === 'production') return 'https://dev.bken.io'
-  return 'http://localhost:3000'
-}
+const getOrigin = (originUrl) => {
+  if (BKEN_ENV === 'dev' && NODE_ENV === 'production') {
+    originUrl = 'https://dev.bken.io';
+  } else if (BKEN_ENV === 'prod' && NODE_ENV === 'production') {
+    originUrl = 'https://bken.io';
+  } else {
+    originUrl = 'http://localhost:3000';
+  }
+
+  console.log({ NODE_ENV, BKEN_ENV });
+  console.log(`originUrl: ${originUrl}`);
+  return originUrl;
+};
 
 const server = new ApolloServer({
   modules: [
@@ -25,10 +30,12 @@ const server = new ApolloServer({
 const app = express();
 
 server.applyMiddleware({
-  app, path: '/api/graphql', cors: {
+  app,
+  path: '/api/graphql',
+  cors: {
     credentials: true,
     origin: getOrigin(),
-  }
+  },
 });
 
 module.exports = app;
