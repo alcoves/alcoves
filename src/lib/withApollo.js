a967d99302625ef49e5b8090108e143e03650a65
Brendan Kennedy <brenwken@gmail.com>
2020-04-16 00:08:15 -0400

    Dynamic envs

--- src/lib/withApollo.js
+++ src/lib/withApollo.js
@@ -8,15 +8,15 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 const getServerUrl = apiUrl => {
-  switch (process.env.BKEN_ENV) {
-    case 'dev':
-      apiUrl = 'https://dev.bken.io/api/graphql';
-      break;
-    case 'prod':
-      apiUrl = 'https://bken.io/api/graphql';
-      break;
-    default:
-      apiUrl = 'http://localhost:4000/api/graphql';
+  const BKEN_ENV = process.env.BKEN_ENV;
+  const NODE_ENV = process.env.NODE_ENV;
+
+  if (BKEN_ENV === 'dev' && NODE_ENV === 'production') {
+    apiUrl = 'https://dev.bken.io/api/graphql';
+  } else if (BKEN_ENV === 'prod' && NODE_ENV === 'production') {
+    apiUrl = 'https://bken.io/api/graphql';
+  } else {
+    apiUrl = 'http://localhost:4000/api/graphql';
   }
 
   console.log(`apiUrl: ${apiUrl}`);
