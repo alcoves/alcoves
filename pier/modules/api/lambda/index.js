57d473bee59a2d1b93797fda6d454739a7711504
Brendan Kennedy <brenwken@gmail.com>
2020-05-11 18:55:31 -0400

    Move api into submodule

---
+++ pier/modules/api/lambda/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server-lambda');
+
+const server = new ApolloServer({
+  modules: [
+    require('./schema/Users'),
+    require('./schema/Videos'),
+    require('./schema/Uploads'),
+  ],
+  context: require('./context'),
+  tracing: true,
+  playground: true,
+  introspection: true,
+});
+
+module.exports.handler = server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
--- pier/stack/dev/main.tf
+++ pier/stack/dev/main.tf
@@ -13,10 +13,7 @@ variable "env" {
   type    = string
 }
 
-module "core" {
-  env                  = var.env
-  source               = "../../modules/core"
-  registry_secrets_arn = "github_registry_login"
-  app_image            = "docker.pkg.github.com/bken-io/api/api:dev"
-  alb_listener_arn     = "arn:aws:elasticloadbalancing:us-east-1:594206825329:listener/app/dev-services/279ca79c2006ab38/555fcd4a863bfde9"
+module "api" {
+  env    = var.env
+  source = "../../modules/api"
 }
