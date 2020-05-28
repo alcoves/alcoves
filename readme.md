1cf8dac1b804ab4354f055e2b65ad0e5e951ded7
Brendan Kennedy <brenwken@gmail.com>
2020-05-27 22:16:00 -0400

    Remove fargate

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
 # bken.io website
 
 ![Build Status](https://github.com/bken-io/web/workflows/build/badge.svg)
-
-This repo contains the single page application for bken.io
--- stack/dev/main.tf
+++ stack/dev/main.tf
@@ -9,7 +9,6 @@ terraform {
 provider "aws" { region  = "us-east-1" }
 
 module "core" {
-  env              = "dev"
-  source           = "../../modules/core"
-  alb_listener_arn = "arn:aws:elasticloadbalancing:us-east-1:594206825329:listener/app/dev-services/279ca79c2006ab38/555fcd4a863bfde9"
+  env    = "dev"
+  source = "../../modules/core"
 }
