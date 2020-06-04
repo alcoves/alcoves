9aeeed0bc6c7bed25bec53f8cf11c868c687bc38
Brendan Kennedy <brenwken@gmail.com>
2020-06-04 10:18:42 -0400

    Fix terraform

--- stack/dev/main.tf
+++ stack/dev/main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     region = "us-east-1"
     bucket = "bken-tf-state"
-    key    = "services/web/dev/tidal.tfstate"
+    key    = "services/web/dev/terraform.tfstate"
   }
 }
 
