9362d1387f685e69292087751885bf6d50768af2
Brendan Kennedy <brenwken@gmail.com>
2020-06-03 21:52:12 -0400

    Add user back to context

--- pier/server/src/context.js
+++ pier/server/src/context.js
@@ -8,20 +8,20 @@ module.exports = (event) => {
   console.log('event', event);
   let user = {};
 
-  // if (event.headers && event.headers.cookie) {
-  //   const cookies = headers.cookie.split(';');
+  if (event.headers && event.headers.cookie) {
+    const cookies = headers.cookie.split(';');
 
-  //   const { idToken } = cookies.reduce((acc, c) => {
-  //     if (c.includes('idToken=')) acc['idToken'] = c.split('idToken=')[1];
-  //     if (c.includes('accessToken='))
-  //       acc['accessToken'] = c.split('accessToken=')[1];
-  //     if (c.includes('refreshToken='))
-  //       acc['refreshToken'] = c.split('refreshToken=')[1];
-  //     return acc;
-  //   }, {});
+    const { idToken } = cookies.reduce((acc, c) => {
+      if (c.includes('idToken=')) acc['idToken'] = c.split('idToken=')[1];
+      if (c.includes('accessToken='))
+        acc['accessToken'] = c.split('accessToken=')[1];
+      if (c.includes('refreshToken='))
+        acc['refreshToken'] = c.split('refreshToken=')[1];
+      return acc;
+    }, {});
 
-  //   if (idToken) user = jwt.decode(idToken);
-  // }
+    if (idToken) user = jwt.decode(idToken);
+  }
 
   return {
     user,
