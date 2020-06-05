b49cbd4b79986a094606160013841d39f7e6c4ea
Brendan Kennedy <brenwken@gmail.com>
2020-06-05 10:45:34 -0400

    Deploy latest image on push

--- stack/dev/main.tf
+++ stack/dev/main.tf
@@ -8,8 +8,10 @@ terraform {
 }
 
 provider "aws" { region = "us-east-1" }
+variable "fargate_image" { type = string }
 
 module "core" {
-  env    = "dev"
-  source = "../../modules/core"
+  env           = "dev"
+  source        = "../../modules/core"
+  fargate_image = var.fargate_image
 }
