67ff40203c2ff4208ebd2312c94a57190a4ef326
Brendan Kennedy <brenwken@gmail.com>
2020-06-08 05:08:37 -0400

    Add basic home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,17 @@ function Index() {
   return (
     <Layout>
       <Navigation />
+      <style jsx>
+        {`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 500px;
+        `}
+      </style>
+      <div>
+        <h1>Simple video sharing</h1>
+      </div>
     </Layout>
   );
 }
