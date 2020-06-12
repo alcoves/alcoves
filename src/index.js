427099488d7eceb57da098755fcb42e4f0d6d292
Brendan Kennedy <brenwken@gmail.com>
2020-06-12 14:59:28 -0400

    Add router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/index';
 
-class Index extends React.Component {
-  render() {
-    return <div>bken.io</div>;
-  }
-}
-
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
