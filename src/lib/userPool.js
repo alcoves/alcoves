6af1b3e4868ef34898510c33730de626eb233f31
Brendan Kennedy <brenwken@gmail.com>
2020-07-02 21:21:03 -0400

    Add BKEN_ENV to user pool config

--- src/lib/userPool.js
+++ src/lib/userPool.js
@@ -1,9 +1,10 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
-// TODO :: Add support for prod
+const { BKEN_ENV } = process.env;
+
 const poolData = {
-  UserPoolId: 'us-east-2_i0Mrl74B8',
-  ClientId: '6loe6isvmd4t1s627a3ujjd4iv',
+  UserPoolId: BKEN_ENV === 'prod' ? 'us-east-2_9tWqgabS8' : 'us-east-2_i0Mrl74B8',
+  ClientId: BKEN_ENV === 'prod' ? '386of4k0jkrlgvd11837k2j0km' : '6loe6isvmd4t1s627a3ujjd4iv',
 };
 
 export default new CognitoUserPool(poolData);
