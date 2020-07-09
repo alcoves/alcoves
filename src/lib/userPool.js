2f571a1d1bc45efaaca9bd0a9dbdd20c3465f922
Brendan Kennedy <brenwken@gmail.com>
2020-07-08 21:35:03 -0400

    Use window to switch api urls

--- src/lib/userPool.js
+++ src/lib/userPool.js
@@ -1,10 +1,17 @@
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
-const { BKEN_ENV } = process.env;
+let poolData;
 
-const poolData = {
-  UserPoolId: BKEN_ENV === 'prod' ? 'us-east-2_9tWqgabS8' : 'us-east-2_i0Mrl74B8',
-  ClientId: BKEN_ENV === 'prod' ? '386of4k0jkrlgvd11837k2j0km' : '6loe6isvmd4t1s627a3ujjd4iv',
-};
+if (window.location.hostname === 'bken.io') {
+  poolData = {
+    UserPoolId: 'us-east-2_9tWqgabS8',
+    ClientId: '386of4k0jkrlgvd11837k2j0km'
+  }
+} else {
+  poolData = {
+    UserPoolId: 'us-east-2_i0Mrl74B8',
+    ClientId: '6loe6isvmd4t1s627a3ujjd4iv'
+  }
+}
 
 export default new CognitoUserPool(poolData);
