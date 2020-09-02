2a109e9ca465983d7a81d43a119d3ac50aa503c3
Brendan Kennedy <brenwken@gmail.com>
2020-09-02 18:36:06 -0400

    Support user registration and login

--- pier/src/models/User.js
+++ pier/src/models/User.js
@@ -5,11 +5,13 @@ const userSchema = new Schema(
   {
     _id: { auto: true, type: Schema.Types.ObjectId },
     password: { type: String, required: true },
-    username: { type: String, required: true },
+    username: { type: String, required: true, lowercase: true },
+    nickname: { type: String, required: false },
     email: {
       type: String,
       unique: true,
       required: true,
+      lowercase: true,
       match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     },
   },
