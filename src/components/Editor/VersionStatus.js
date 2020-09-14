190d2e3768c290380a834c0c3ac4868b501fa2b7
Brendan Kennedy <brenwken@gmail.com>
2020-09-14 12:58:39 -0400

    Update tidal schema components

--- src/components/Editor/VersionStatus.js
+++ src/components/Editor/VersionStatus.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import { Grid, LinearProgress, Paper, Typography } from '@material-ui/core';
 
-function timeConversion(startTime, completeTime) {
-  const millisec = new Date(completeTime).getTime() - new Date(startTime).getTime();
-  const seconds = (millisec / 1000).toFixed(1);
-  const minutes = (millisec / (1000 * 60)).toFixed(1);
-  const hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-  const days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-  if (seconds < 60) {
-    return `${seconds} Sec`;
-  }
-  if (minutes < 60) {
-    return `${minutes} Min`;
-  }
-  if (hours < 24) {
-    return `${hours} Hrs`;
-  }
-  return `${days} Days`;
-}
-
 export default ({ versions }) => {
   return (
     <Grid spacing={1} container>
