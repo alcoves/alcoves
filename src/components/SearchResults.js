53c1c2920f450bef92dfdfb864c35534102785a4
Brendan Kennedy <brenwken@gmail.com>
2020-09-21 14:02:05 -0400

    Add links to videos on search result page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 import React, { useContext } from 'react';
 import { SearchContext } from '../contexts/SearchContext';
 import { Container, LinearProgress, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 export default function searchResults() {
   const { error, data, loading } = useContext(SearchContext);
@@ -14,11 +15,18 @@ export default function searchResults() {
           console.log('v', v);
           return <div key={v.id} style={{ display: 'flex', flexDirection: 'row' }}>
             <div style={{ marginRight: '10px' }}>
-              <img style={{ width: 350, borderRadius: '3px' }} src={v.thumbnails[0]} />
+              <Link to={`/v/${v.id}`}>
+                <img style={{ width: 350, borderRadius: '3px' }} src={v.thumbnails[0]} />
+              </Link>
             </div>
             <div>
-              <Typography variant="h4">{v.title}</Typography>
-              <Typography variant="subtitle2">{v.user.username} - {moment(parseInt(v.createdAt)).fromNow()}</Typography>
+              <Link to={`/v/${v.id}`} >
+                <Typography variant="h4">{v.title}</Typography>
+              </Link>
+              <Typography variant="subtitle2">{moment(parseInt(v.createdAt)).fromNow()}</Typography>
+              <Link to={`/v/${v.user.username}`} >
+                <Typography variant="subtitle2">{v.user.username}</Typography>
+              </Link>
             </div>
           </div>
         })}
