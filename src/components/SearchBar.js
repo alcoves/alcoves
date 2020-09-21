f0a0b3a987dcacddf7cde52bb4b7ce525c819405
Brendan Kennedy <brenwken@gmail.com>
2020-09-21 16:40:43 -0400

    Center search box

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,27 +3,23 @@ import React, { useContext, useState } from 'react';
 import InputBase from '@material-ui/core/InputBase';
 import { SearchContext } from '../contexts/SearchContext';
 
-const SearchBox = styled.div`
+const SearchBox = styled(InputBase)`
   width: 100%;
   height: 30px;
-  font-size: .9em;
-  min-width: 200px;
+  max-width: 400px;
   border-radius: 3px;
   background-color: #2f3438;
+  font-size: .9em !important;
   padding: 0px 10px 0px 10px;
-`
+`;
 
 export default function search() {
   const { search, setSearch } = useContext(SearchContext);
   const [localSearch, setLocalSearch] = useState(search);
 
-  return <SearchBox >
-    <InputBase
-      fullWidth
-      value={localSearch}
-      placeholder='Search'
-      onChange={(e) => { setLocalSearch(e.target.value) }}
-      onKeyPress={(e) => { if (e.key === 'Enter') { setSearch(e.target.value) } }}
-    />
-  </SearchBox>
+  return <SearchBox
+    value={localSearch}
+    placeholder='Search'
+    onChange={(e) => { setLocalSearch(e.target.value) }}
+    onKeyPress={(e) => { if (e.key === 'Enter') { setSearch(e.target.value) } }} />
 }
