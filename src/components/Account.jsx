e9c50103530c0286dc57041cabd8e6b568975ddb
Brendan Kennedy <brenwken@gmail.com>
2020-10-24 20:37:39 -0400

    Add initial avatar rendering on account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,9 +1,21 @@
-import React, { useContext, } from 'react';
-import { useHistory, } from 'react-router-dom';
-import {
-  Button, LinearProgress, Typography, Container,
-} from '@material-ui/core';
-import { UserContext, } from '../contexts/UserContext';
+import styled from 'styled-components';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Button, LinearProgress, Typography, Container } from '@material-ui/core';
+import { UserContext } from '../contexts/UserContext';
+
+const AvatarContainer = styled.div`
+  display: flex;
+  padding-top: 20px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Avatar = styled.img`
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+`;
 
 export default function Account() {
   const history = useHistory();
@@ -12,19 +24,29 @@ export default function Account() {
   if (user) {
     return (
       <Container maxWidth='xs'>
-        <Typography varient='subtitle2'>{`you are logged in as ${user.email}`}</Typography>
+        <AvatarContainer>
+          <Avatar
+            src='https://s3.us-east-2.wasabisys.com/cdn.bken.io/files/default-thumbnail-sm.jpg'
+            alt='avatar'
+          />
+        </AvatarContainer>
+        <br />
+        <Typography align='center' variant='subtitle1'>{`${user.username}`}</Typography>
+        <Typography align='center' variant='subtitle2'>{`${user.email}`}</Typography>
+        <br />
         <Button
+          fullWidth
+          variant='outlined'
           onClick={() => {
             logout();
             history.push('/');
-          }}
-        >
-          Sign Out
+          }}>
+          Log Out
         </Button>
       </Container>
     );
   }
 
-  if (!user) return history.push('/login');
+  // if (!user) return history.push('/login');
   return <LinearProgress />;
 }
