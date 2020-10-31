8482df94c206653f6e230a02a567073d0940d58c
Brendan Kennedy <brenwken@gmail.com>
2020-10-31 10:25:24 -0400

    Add upload avatar test

--- pier/src/modules/users/resolvers/uploadAvatar.js
+++ pier/src/modules/users/resolvers/uploadAvatar.js
@@ -6,24 +6,30 @@ const ws3 = require('../../../utils/ws3');
 
 const { WASABI_BUCKET } = require('../../../utils/config');
 
+async function processAvatar(tmpFile) {
+  const avatar = await sharp(tmpFile).resize({
+    width: 300,
+    height: 300,
+  }).webp({
+    quality: 80,
+    reductionEffort: 6,
+  }).toBuffer();
+  return avatar;
+}
+
 async function uploadAvatar(_, args, { user, authenticate }) {
   authenticate();
   const { createReadStream, filename } = await args.file;
 
   const uploadKey = `avatars/${user.id}/${nanoid()}.webp`;
+  const url = `https://cdn.bken.io/${uploadKey}`;
   const tmpDir = fs.mkdtempSync('/tmp/');
   const tmpFile = `${tmpDir}/${filename}`;
   const tmpWriteStream = createReadStream().pipe(fs.createWriteStream(tmpFile));
 
   return new Promise((resolve) => {
     tmpWriteStream.on('finish', async () => {
-      const avatar = await sharp(tmpFile).resize({
-        width: 300,
-        height: 300,
-      }).webp({
-        quality: 80,
-        reductionEffort: 6,
-      }).toBuffer();
+      const avatar = await processAvatar(tmpFile);
 
       const { Contents } = await ws3.listObjectsV2({
         Bucket: WASABI_BUCKET,
@@ -42,7 +48,6 @@ async function uploadAvatar(_, args, { user, authenticate }) {
         ContentType: 'image/webp',
       }).promise();
 
-      const url = `https://cdn.bken.io/${uploadKey}`;
       await User.findByIdAndUpdate(user.id, { avatar: url });
       resolve({ url });
     });
@@ -50,3 +55,4 @@ async function uploadAvatar(_, args, { user, authenticate }) {
 }
 
 module.exports = uploadAvatar;
+module.exports.processAvatar = processAvatar;
