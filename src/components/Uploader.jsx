4aa09676152b4547d708a55b29ca8d6f6714de08
Brendan Kennedy <brenwken@gmail.com>
2020-11-01 19:44:05 -0500

    Multiple video uploads

--- src/components/Uploader.jsx
+++ src/components/Uploader.jsx
@@ -1,98 +1,121 @@
 import axios from 'axios';
 import styled from 'styled-components';
 
-import {useDropzone,} from 'react-dropzone';
-import { useHistory, } from 'react-router-dom';
-import { useMutation, } from '@apollo/client';
-import React, { useState, useEffect, useCallback, } from 'react';
-import { LinearProgress, Container, CircularProgress, Typography,
-} from '@material-ui/core';
+import { useDropzone } from 'react-dropzone';
+import { useHistory } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Button, LinearProgress, Container, Typography } from '@material-ui/core';
 
 import createUploadQuery from '../gql/createUpload';
 import completeUploadQuery from '../gql/completeUpload';
 
-function UploadProgress({ id, url, file }) {
+function UploadProgress({ file }) {
   const history = useHistory();
   const [progress, setProgress] = useState(0);
 
-  const [completeUpload, { loading, data, error }] = useMutation(completeUploadQuery);
+  const [createUpload, { called: createUploadCalled, data: createUploadData }] = useMutation(
+    createUploadQuery,
+  );
+  const [completeUpload, { data: completeUploadData, error: completeUploadError }] = useMutation(
+    completeUploadQuery,
+  );
 
   useEffect(() => {
-    if (data) history.push(`/editor/${id}`);
-  });
-
-  useEffect(() => {
-    axios
-      .put(url, file, {
-        headers: { 'Content-Type': file.type },
-        onUploadProgress: (e) => {
-          const currentProgress = (e.loaded / file.size) * 100;
-          console.log(currentProgress);
-          setProgress(currentProgress);
-        },
-      })
-      .then(() => {
-        // Load video to get duration
-        const video = document.createElement('video');
-        video.setAttribute('src', window.URL.createObjectURL(file));
-        video.onloadeddata = (event) => {
-          const meta = event.srcElement; // TODO :: This is deprecated
-          completeUpload({
-            variables: {
-              input: {
-                id, title: file.name, duration: meta.duration, fileType: file.type,
-              },
-            },
-          });
-        };
-      });
+    if (!createUploadCalled && !createUploadData) {
+      createUpload({ variables: { input: { fileType: file.type } } });
+    }
   }, []);
 
-  if (loading) return <CircularProgress />;
-  if (error) return <div>{JSON.stringify(error)}</div>;
-  return <LinearProgress variant='determinate' value={progress} />;
+  useEffect(() => {
+    if (createUploadData) {
+      axios
+        .put(createUploadData.createUpload.url, file, {
+          headers: { 'Content-Type': file.type },
+          onUploadProgress: e => {
+            const currentProgress = (e.loaded / file.size) * 100;
+            console.log(currentProgress);
+            setProgress(currentProgress);
+          },
+        })
+        .then(() => {
+          // Load video to get duration
+          const video = document.createElement('video');
+          video.setAttribute('src', window.URL.createObjectURL(file));
+          video.onloadeddata = event => {
+            const meta = event.srcElement; // TODO :: This is deprecated
+            completeUpload({
+              variables: {
+                input: {
+                  title: file.name,
+                  fileType: file.type,
+                  duration: meta.duration,
+                  id: createUploadData.createUpload.id,
+                },
+              },
+            });
+          };
+        });
+    }
+  }, [createUploadData]);
+
+  if (completeUploadError) return <div>{JSON.stringify(completeUploadError)}</div>;
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant='subtitle1'>{file.name}</Typography>
+        {createUploadData && (
+          <Button
+            onClick={() => {
+              history.push(`/editor/${createUploadData.createUpload.id}`);
+            }}>
+            Edit
+          </Button>
+        )}
+      </div>
+      <LinearProgress variant='determinate' value={progress} />
+    </div>
+  );
 }
 
 const Dropzone = styled.div`
-  width: 100%
-  height: 200px;
+  display: flex;
+  width: 100%;
+  height: 100px;
+  cursor: pointer;
   border-radius: 4px;
-  border: dashed 2px grey;
+  align-items: center;
+  justify-content: center;
+  border: dashed 3px grey;
 `;
 
 function SimpleUploader() {
-  const [file, setFile] = useState(null);
-  const [createUpload, { called, loading, data }] = useMutation(createUploadQuery);
+  const [files, setFiles] = useState([]);
 
   const onDrop = useCallback(acceptedFiles => {
-    setFile(acceptedFiles[0]);
+    setFiles(acceptedFiles);
   }, []);
 
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-  } = useDropzone({ 
-    onDrop, 
-    accept: 'video/mp4', 
-    disabled: Boolean(loading || called),
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: 'video/mp4',
+    disabled: Boolean(files.length),
   });
-
-  useEffect(() => {
-    if (file) createUpload({ variables: { input: { fileType: file.type } } });
-  }, [file]);
 
   return (
     <Container maxWidth='xs' style={{ paddingTop: '20px' }}>
       <Dropzone {...getRootProps()}>
         <input {...getInputProps()} />
-        { isDragActive ?
-          <Typography variant='body1'> Drop here </Typography> :
-          <Typography variant='body1'> Select a Video </Typography>}
+        {isDragActive ? (
+          <Typography variant='subtitle1'> Drop here! </Typography>
+        ) : (
+          <Typography variant='subtitle1'> Upload </Typography>
+        )}
       </Dropzone>
-      {data && file && (
-        <UploadProgress id={data.createUpload.id} url={data.createUpload.url} file={file} />
-      )}
+      {files.map(file => {
+        return <UploadProgress key={file.name} file={file} />;
+      })}
     </Container>
   );
 }
