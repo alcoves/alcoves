0dd5dfa7306848f82ae9e7f54f15bdd838efaa21
Brendan Kennedy <brenwken@gmail.com>
2020-12-04 04:58:58 -0500

    Remove Mui theme

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,5 +1,5 @@
 import Layout from '../components/Layout';
 
-const ExploreView = props => <Layout>Account Page</Layout>;
+const ExploreView = () => <Layout>Account Page</Layout>;
 
 export default ExploreView;
