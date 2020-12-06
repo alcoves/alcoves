25fd36d8edd9f1b05667bc7aae0a56bc28cef48e
Brendan Kennedy <brenwken@gmail.com>
2020-12-06 15:00:16 -0500

    Working on video page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,22 @@
+import { Heading, Pane, } from 'evergreen-ui';
 import React from 'react';
 import Layout from '../components/Layout';
 
-const Index = () => <Layout />;
+function index() {
+  return (
+    <>
+      <Layout>
+        <Pane
+          display='flex'
+          justifyContent='center'
+        >
+          <Heading size={500}>
+            We're revamping this page, sit tight!
+          </Heading>
+        </Pane>
+      </Layout>
+    </>
+  );
+}
 
-export default Index;
+export default index;
