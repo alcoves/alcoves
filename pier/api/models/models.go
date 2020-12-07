a886f5651638cbb413429b56d423af25762ecc93
Brendan Kennedy <brenwken@gmail.com>
2020-12-07 07:38:26 -0500

    Add thumbnail attribute to video

--- pier/api/models/models.go
+++ pier/api/models/models.go
@@ -20,6 +20,7 @@ type Video struct {
 	User       string  `gorm:"index" json:"user"`
 	Views      int     `gorm:"default:0" json:"views"`
 	Visibility string  `gorm:"default:unlisted" json:"visibility"`
+	Thumbnail  string  `gorm:"default:https://cdn.bken.io/files/default-thumbnail-sm.jpg" json:"thumbnail"`
 }
 
 // VideoView 🎥
