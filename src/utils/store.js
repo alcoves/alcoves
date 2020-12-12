095b7781efaa6ce80a8059aa67fd170d445bb11b
Brendan Kennedy <brenwken@gmail.com>
2020-12-12 15:13:02 -0500

    Cleanup the store

--- src/utils/store.js
+++ src/utils/store.js
@@ -6,40 +6,34 @@ const Context = createContext();
 function Provider({ children }) {
   const [store, setStore] = useState({
     user: null,
-    search: null,
     authenticated: false,
-  });
-
-  function login(token) {
-    if (token) localStorage.setItem('token', token);
-    const decoded = jwt.decode(localStorage.getItem('token'));
-
-    if (decoded) {
-      // TODO :: Check for token expiry
-      console.log('loaded user successfully');
+    login(token) {
+      if (token) localStorage.setItem('token', token);
+      const decoded = jwt.decode(localStorage.getItem('token'));
+  
+      if (decoded) {
+        // TODO :: Check for token expiry
+        console.log('loaded user successfully');
+        setStore({
+          ...store,
+          user: { ...decoded },
+          authenticated: true,
+        });
+      }
+    },
+    logout() {
+      console.log('logging out user');
+      localStorage.removeItem('token');
       setStore({
         ...store,
-        user: { ...decoded },
-        authenticated: true,
+        user: null,
+        authenticated: false,
       });
-    }
-  }
+    },
+  });
 
-  function logout() {
-    console.log('logging out user');
-    localStorage.removeItem('token');
-    setStore({
-      ...store,
-      user: null,
-      authenticated: false,
-    });
-  }
-
-  useEffect(() => {
-    if (!store.user) login();
-  }, []);
-
-  return <Context.Provider value={{ ...store, logout, login }}>{children}</Context.Provider>;
+  useEffect(() => { if (!store.user) store.login(); }, []);
+  return <Context.Provider value={store}>{children}</Context.Provider>;
 }
 
 export { Context, Provider };
