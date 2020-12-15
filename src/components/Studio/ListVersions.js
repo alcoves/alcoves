2630c8a27a216846f5248e427cea7c0e981be888
Brendan Kennedy <brenwken@gmail.com>
2020-12-14 20:47:02 -0500

    Add processing icons

--- src/components/Studio/ListVersions.js
+++ src/components/Studio/ListVersions.js
@@ -1,17 +1,42 @@
 import { Text, Heading, Pane, Spinner, } from 'evergreen-ui';
 import { useEffect, } from 'react';
+import styled from 'styled-components';
 import { useApi,} from '../../utils/api';
+import Icon from '../Icon';
 
 let timer;
+
+const ColoredIcon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: ${p => p.from};
+  animation: change_color 2.5s infinite;
+
+  @keyframes change_color {
+    from { color: ${p => p.from}; }
+    to { color: ${p => p.to}; transform:rotate(360deg); }
+  }
+`;
 
 function Version({ version }) {
   const { name, status, percentCompleted } = version;
   return (
-    <Pane minWidth={200}>
-      <Heading size={300}>   
-        {name}
-      </Heading>
-      <Text>{`${percentCompleted}% ${status}`}</Text>
+    <Pane minWidth={200} display='flex'>
+      {status === 'completed' ? (
+        <Icon color='green' name='check-circle' />
+      ) : (
+        <ColoredIcon from='orange' to='orange'>
+          <Icon name='settings' />
+        </ColoredIcon>
+      )}
+      <Pane display='flex' flexDirection='column'>
+        <Heading size={400}>
+          {name}
+        </Heading>
+        <Text>{`${percentCompleted}%`}</Text>
+      </Pane>
     </Pane>
   );
 }
@@ -36,7 +61,7 @@ export default function ListVersions({ id }) {
         <Heading size={500}>
           Versions
         </Heading>
-        <Pane display='flex' flexDirection='column'>  
+        <Pane display='flex' flexDirection='column'>
           {data.map(v => <Version key={v.name} version={v} />)}
         </Pane>
       </Pane>
