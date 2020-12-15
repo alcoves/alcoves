78cd52f0ee1a373e3bf529680ffce7adb9daf371
Brendan Kennedy <brenwken@gmail.com>
2020-12-14 21:13:55 -0500

    Cleanup studio view

--- src/pages/studio/index.js
+++ src/pages/studio/index.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 import Layout from '../../components/Layout';
 import { Context, } from '../../utils/store';
 import { useApiLazy, } from '../../utils/api';
+import Icon from '../../components/Icon';
 
 export default function studio() {
   const router = useRouter();
@@ -23,14 +24,15 @@ export default function studio() {
         <Pane padding={10}>
           <Table>
             <Table.Head>
-              <Table.TextHeaderCell>
+              <Table.TextHeaderCell flexBasis={120} flexShrink={0} flexGrow={0} />
+              <Table.TextHeaderCell flexBasis={75} flexShrink={0} flexGrow={0}>
+                Visibility
+              </Table.TextHeaderCell>
+              <Table.TextHeaderCell flexBasis={560} flexShrink={0} flexGrow={0}>
                 Title
               </Table.TextHeaderCell>
               <Table.TextHeaderCell>
                 Created At
-              </Table.TextHeaderCell>
-              <Table.TextHeaderCell>
-                Visibility
               </Table.TextHeaderCell>
               <Table.TextHeaderCell>
                 Views
@@ -38,12 +40,40 @@ export default function studio() {
             </Table.Head>
             <Table.Body height='100%'>
               {data.map(v => (
-                <Table.Row key={v.id} isSelectable onSelect={() => router.push(`/studio/${v.id}`)}>
-                  <Table.TextCell>{v.title}</Table.TextCell>
-                  <Table.TextCell>{moment(v.createdAt).fromNow()}</Table.TextCell>
-                  <Table.TextCell>
-                    {v.visibility}
+                <Table.Row
+                  key={v.id}
+                  isSelectable
+                  height='50px'
+                  onSelect={() => router.push(`/studio/${v.id}`)}
+                >
+                  <Table.TextCell
+                    flexBasis={120}
+                    flexShrink={0}
+                    flexGrow={0}
+                  >
+                    <img height='50px' src={v.thumbnail} alt='thumb' />
                   </Table.TextCell>
+                  <Table.TextCell flexBasis={75} flexShrink={0} flexGrow={0}>
+                    {v.visibility === 'public' ? (
+                      <Icon
+                        width='20px'
+                        height='20px'
+                        name='globe'
+                        color='green'
+                      />
+                    ) : (
+                      <Icon
+                        width='20px'
+                        height='20px'
+                        name='link-2'
+                        color='grey'
+                      />
+                    )}
+                  </Table.TextCell>
+                  <Table.TextCell flexBasis={560} flexShrink={0} flexGrow={0}>
+                    {v.title}
+                  </Table.TextCell>
+                  <Table.TextCell>{moment(v.createdAt).fromNow()}</Table.TextCell>
                   <Table.TextCell isNumber>
                     {v.views}
                   </Table.TextCell>
