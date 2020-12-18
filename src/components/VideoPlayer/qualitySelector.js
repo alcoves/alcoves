b24fbfa8a0f44dc6f0e66e56947af1ed99e6503f
Brendan Kennedy <brenwken@gmail.com>
2020-12-18 17:57:34 -0500

    Add shortcut to pause video

--- src/components/VideoPlayer/qualitySelector.js
+++ src/components/VideoPlayer/qualitySelector.js
@@ -1,61 +1,36 @@
-import React, { useState, } from 'react';
-import { Text, Menu, Popover, } from 'evergreen-ui';
+import React from 'react';
+import { Menu, } from 'grommet';
 import Icon from '../Icon';
 
 function QualitySelector({ hls }) {
-  const [open, setOpen] = useState(false);
+  function QualityIcon() {
+    return (
+      <Icon
+        width={20}
+        height={20}
+        stroke='#fff'
+        name='settings'
+        cursor='pointer'
+      />
+    );
+  }
 
-  function toggleOpen() {
-    setOpen(!open);
+  function handleLevel(i) {
+    console.log('setting load level', i);
+    hls.currentLevel = i;
   }
 
   if (!hls?.levels?.length) return <div />;
 
   return (
-    <Popover
-      isShown={open}
-      position='top'
-      content={(
-        <Menu>
-          <Menu.Group>
-            {hls.levels.map((l, i) => (
-              <Menu.Item
-                value={i}
-                key={l.name}
-                onSelect={() => {
-                  console.log('setting load level', i);
-                  hls.currentLevel = i;
-                  setOpen(false);
-                }}
-              >
-                <Text fontWeight={hls.currentLevel === i ? 500 : 300}>
-                  {l.name}
-                </Text>
-              </Menu.Item>
-            ))}
-            <Menu.Item onSelect={() =>{
-              hls.currentLevel = -1;
-              setOpen(false);
-            }}
-            >
-              Auto
-            </Menu.Item>
-          </Menu.Group>
-        </Menu>
-      )}
-    >
-      <div>
-        <Icon
-          width={20}
-          height={20}
-          stroke='#fff'
-          name='settings'
-          cursor='pointer'
-          onClick={toggleOpen}
-        />
-      </div>
-    </Popover>
+    <Menu
+      icon={QualityIcon()}
+      dropAlign={{ bottom: 'top', right: 'right' }}
+      items={hls.levels.map((l, i) => ({ label: l.name, onClick: () => handleLevel(i) }))}
+    />
   );
 }
+
+
 
 export default QualitySelector;
