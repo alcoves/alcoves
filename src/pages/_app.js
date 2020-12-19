8173808e71b5c49c810dce953555a5bc7878d7b5
Brendan Kennedy <brenwken@gmail.com>
2020-12-18 23:14:29 -0500

    Add Logrocket

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import '../styles/index.css';
 import Head from 'next/head';
 import { Grommet, } from 'grommet';
 import React from 'react';
+import LogRocket  from 'logrocket';
+import setupLogRocketReact  from 'logrocket-react';
 import Bugsnag from '../utils/bugsnag';
 import theme from '../styles/theme';
 import { Provider, } from '../utils/store';
@@ -9,6 +11,11 @@ import { Provider, } from '../utils/store';
 const ErrorBoundary = Bugsnag.getPlugin('react');
 
 function App({ Component, pageProps }) {
+  if (typeof window !== 'undefined') {
+    LogRocket.init('7qbcw4/bkenio');
+    setupLogRocketReact(LogRocket);
+  }
+
   return (
     <ErrorBoundary>
       <Head>
