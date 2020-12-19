c3c23dfdb44278cfe058d6738993d609b0c75cb9
Brendan Kennedy <brenwken@gmail.com>
2020-12-19 14:44:18 -0500

    🔧 Styled components SSR

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,15 @@ function App({ Component, pageProps }) {
     return (
       <ErrorBoundary>
         <Head>
-          <title>bken.io</title>
+          <link rel='shortcut icon' href='./favicon.ico' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap' rel='stylesheet' />
+          <meta name='version' content='%REACT_APP_GIT_SHA%' />
+          <meta property='og:title' content='bken.io' />
+          <meta property='og:url' content='https://bken.io' />
+          <meta property='og:image' content='./favicon.ico' />
+          <meta property='og:description' content='bken.io is a video sharing platform' />
+          <script src='https://cdn.jsdelivr.net/npm/hls.js@latest' />
         </Head>
         <Grommet full theme={theme} themeMode='dark'>
           <Provider>
