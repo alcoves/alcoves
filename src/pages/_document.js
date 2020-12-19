085e08940b7863516f805147e66f3b92f1a10383
Brendan Kennedy <brenwken@gmail.com>
2020-12-19 03:26:38 -0500

    💄 Add server side rendering of styled-components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript, } from 'next/document';
+import { ServerStyleSheet, } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+    const styleTags = sheet.getStyleElement();
+    return {
+      ...page, styleTags,
+    };
+  }
+
+  
   render() {
     return (
       <Html lang='en'>
         <Head>
+          {this.props.styleTags}
           <link rel='shortcut icon' href='./favicon.ico' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap' rel='stylesheet' />
