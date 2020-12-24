8b492c008c679fdf52601584b745cef6970aaf99
Brendan Kennedy <brenwken@gmail.com>
2020-12-23 21:42:53 -0500

    ♻ Cleaning

--- src/utils/bugsnag.js
+++ src/utils/bugsnag.js
@@ -3,7 +3,7 @@ import Bugsnag from '@bugsnag/js';
 import LogRocket from 'logrocket';
 import BugsnagPluginReact from '@bugsnag/plugin-react';
 
-if ( process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   Bugsnag.start({
     apiKey: '5afa8cfcc40ed672105826a20f3dcc05',
     plugins: [new BugsnagPluginReact(React)],
