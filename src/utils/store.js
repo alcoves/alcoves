949ee034c0bc78bc289d2ecafcae8a26262bacac
Brendan Kennedy <brenwken@gmail.com>
2021-01-05 13:34:17 -0500

    Auto log user out when their token expires.

--- src/utils/store.js
+++ src/utils/store.js
@@ -9,48 +9,54 @@ function Provider({ children }) {
     user: null,
     loading: true,
     authenticated: false,
-    login(token) {
-      if (token) localStorage.setItem('token', token);
-      const decoded = jwt.decode(localStorage.getItem('token'));
-  
-      if (decoded) {
-        // TODO :: Check for token expiry
-        if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
-          LogRocket.identify(decoded.id, {
-            email: decoded.email,
-            name: decoded.username,
-            username: decoded.username,
-          });
-        }
+  });
 
-        setStore({
-          ...store,
-          user: { ...decoded },
-          loading: false,
-          authenticated: true,
-        });
-      } else {
-        setStore({
-          ...store,
-          loading: false,
-          authenticated: false,
+  function logout() {
+    console.log('logging out user');
+    localStorage.removeItem('token');
+    setStore({
+      ...store,
+      user: null,
+      loading: false,
+      authenticated: false,
+    });
+  };
+
+  function login(token) {
+    if (token) localStorage.setItem('token', token);
+    const decoded = jwt.decode(localStorage.getItem('token'));
+
+    if (decoded) {
+      if (Date.now() / 1000 > decoded.exp) {
+        logout();
+        return;
+      }
+
+      if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
+        LogRocket.identify(decoded.id, {
+          email: decoded.email,
+          name: decoded.username,
+          username: decoded.username,
         });
       }
-    },
-    logout() {
-      console.log('logging out user');
-      localStorage.removeItem('token');
+
       setStore({
         ...store,
-        user: null,
+        user: { ...decoded },
+        loading: false,
+        authenticated: true,
+      });
+    } else {
+      setStore({
+        ...store,
         loading: false,
         authenticated: false,
       });
-    },
-  });
+    }
+  };
 
-  useEffect(() => { if (!store.user) store.login(); }, []);
-  return <Context.Provider value={store}>{children}</Context.Provider>;
+  useEffect(() => { if (!store.user) login(); }, []);
+  return <Context.Provider value={{...store, login, logout}}>{children}</Context.Provider>;
 }
 
 export { Context, Provider };
