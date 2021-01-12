80651ceb83ac87a9cb4538e4d6d237328358845e
Brendan Kennedy <brenwken@gmail.com>
2021-01-12 12:30:02 -0500

    🌟 Add video player to studio edit page

--- src/pages/studio/index.js
+++ src/pages/studio/index.js
@@ -104,7 +104,7 @@ export default function studio() {
                       />
                     </svg>
                   ) : (
-                    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                    <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='#eee'>
                       <path
                         strokeLinecap='round'
                         strokeLinejoin='round'
