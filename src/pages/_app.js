62b7138188f6cf4ae4c4618219f7bc3da7468228
Brendan Kennedy <brenwken@gmail.com>
2021-01-13 15:39:06 -0500

    💄 Change to Nunito Sans font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,17 +17,6 @@ function App({ Component, pageProps }) {
     const ErrorBoundary = Bugsnag.getPlugin('react');
     return (
       <ErrorBoundary>
-        <Head>
-          <link rel='shortcut icon' href='./favicon.ico' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap' rel='stylesheet' />
-          <meta name='version' content='%REACT_APP_GIT_SHA%' />
-          <meta property='og:title' content='bken.io' />
-          <meta property='og:url' content='https://bken.io' />
-          <meta property='og:image' content='./favicon.ico' />
-          <meta property='og:description' content='bken.io is a video sharing platform' />
-          <script src='https://cdn.jsdelivr.net/npm/hls.js@latest' />
-        </Head>
         <Provider>
           <Component {...pageProps} />
         </Provider>
