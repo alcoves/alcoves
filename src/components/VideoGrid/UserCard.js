58495b43f12c19e61e69e426ce741d8b263082bd
Brendan Kennedy <brenwken@gmail.com>
2021-01-14 09:53:25 -0500

    🔥 Remove video meta loading state

--- src/components/VideoGrid/UserCard.js
+++ src/components/VideoGrid/UserCard.js
@@ -1,20 +1,8 @@
 
 import { useApi, } from '../../utils/api';
-import Spinner from '../Spinner';
 import VideoMeta from './VideoMeta';
 
 export default function UserVideoCard({ v }) {
   const { data } = useApi(`/users/${v.userId}`);
-
-  if (data) {
-    return (
-      <VideoMeta v={v} u={data} />
-    );
-  }
-
-  return (
-    <div className='flex flex-row w-full'>
-      <Spinner />
-    </div>
-  ); 
+  return <VideoMeta v={v} u={data} />;
 }
