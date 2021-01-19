5b89fa9b66a496dfb0a472cbc81d55a0b7a963e3
Brendan Kennedy <brenwken@gmail.com>
2021-01-18 21:36:24 -0500

    🐛 Display correct HLS level

--- src/components/VideoPlayer/qualitySelector.js
+++ src/components/VideoPlayer/qualitySelector.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function QualitySelector({ hls }) {
   function handleLevel(i) {
     console.log('setting load level', i);
@@ -16,14 +14,17 @@ function QualitySelector({ hls }) {
       className='cursor-pointer bg-transparent text-gray-200 text-sm w-full font-semibold rounded-md'
     >
       {hls.levels.map((l, i) => (
-        <option key={l.name} className='bg-gray-800' value={i}>
+        <option
+          value={i}
+          key={l.name}
+          className='bg-gray-800'
+          selected={hls.currentLevel === i}
+        >
           {l.name}
         </option>
       ))}
     </select>
   );
 }
-
-
 
 export default QualitySelector;
