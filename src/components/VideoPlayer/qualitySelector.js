06284d759f6c30feaa8f52a1a86ef538e80bf427
Brendan Kennedy <brenwken@gmail.com>
2021-01-18 22:31:25 -0500

    🐛 Fix selected menu react warning

--- src/components/VideoPlayer/qualitySelector.js
+++ src/components/VideoPlayer/qualitySelector.js
@@ -1,24 +1,21 @@
 function QualitySelector({ hls }) {
-  function handleLevel(i) {
-    console.log('setting load level', i);
-    hls.currentLevel = i;
-  }
-
   if (!hls?.levels?.length) return <div />;
 
   return (
     <select
       id='visibility'
       name='visibility'
-      onChange={(e) => handleLevel(e.target.value)}
+      defaultValue={hls.currentLevel}
       className='cursor-pointer bg-transparent text-gray-200 text-sm w-full font-semibold rounded-md'
+      onChange={(e) => {
+        hls.currentLevel = e.target.value;
+      }}
     >
       {hls.levels.map((l, i) => (
         <option
           value={i}
           key={l.name}
           className='bg-gray-800'
-          selected={hls.currentLevel === i}
         >
           {l.name}
         </option>
