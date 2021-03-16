8b563c6c427e055ab5f0d6bb5b8b15aeeb7b860d
Brendan Kennedy <brenwken@gmail.com>
2021-03-15 20:23:36 -0400

    🔒 Initial NextAuth.js work

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,7 +19,7 @@ export default function Navigation() {
         <>
         </>
       );
-    } if (!loading && session.user) {
+    } if (!loading && session && session.user) {
       return(
         <>
           <svg
@@ -70,23 +70,6 @@ export default function Navigation() {
         </>
       );
     }
-    return (
-      <svg
-        onClick={() => router.push('/login')} 
-        xmlns='http://www.w3.org/2000/svg'
-        fill='none'
-        className='cursor-pointer h-8 w-8 mx-2'
-        stroke='white'
-        viewBox='0 0 24 24'
-      >
-        <path
-          strokeLinecap='round'
-          strokeLinejoin='round'
-          strokeWidth={2}
-          d='M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-        />
-      </svg>
-    );
   }
 
   return (
---
+++ src/pages/v/[id].js
@@ -0,0 +1,68 @@
+import Head from 'next/head';
+import moment from 'moment';
+import Layout from '../../components/Layout';
+import VideoPlayer from '../../components/VideoPlayer/index';
+import abbreviateNumber from '../../utils/abbreviateNumber';
+import VideoPageUserCard from '../../components/VideoPageUserCard';
+import Spinner from '../../components/Spinner';
+
+import useSWR from 'swr'
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+export default function Video(props) {
+  // const router = useRouter();
+  // const [watchVideo, { called: watchVideoCalled }] = useApiLazy();
+  // useEffect(() => {
+  //   if (video && id && !watchVideoCalled) {
+  //     watchVideo({ method: 'post', url: `/videos/${id}/views` });
+  //   } 
+  // }, [video]);
+
+  const { data } = useSWR(props.url, fetcher, { initialData: props.video })
+  console.log('data', data);
+
+  if (data) {
+    const subHeader = `${
+      abbreviateNumber(data.views)} views ·
+      ${moment(data.createdAt).fromNow()} · 
+      ${data.visibility}
+    `;
+
+    return (
+      <div>
+        <Head>
+          <title>{data.title}</title>
+          <meta property='og:title' content={data.title} />
+          <meta property='og:image' content={data.thumbnail} />
+          <meta property='og:description' content={data.title} />
+        </Head>
+        <Layout>
+          <div>
+            <VideoPlayer url={data.hls_master_link} />
+            <div className='m-3 flex flex-col'>
+              <h1 className='text-3xl font-semibold text-gray-200'>{data.title}</h1>
+              <p className='text-sm font-semibold text-gray-400'>
+                {subHeader}
+              </p>
+              <VideoPageUserCard id={data.userId} />
+            </div>
+          </div>
+        </Layout>
+      </div>
+    );
+  }
+
+  return (
+    <Layout>
+      <div align='center'>
+        <Spinner />
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const url = `http://localhost:3000/api/videos/${params.id}`
+  const video = await fetcher(url)
+  return { props: { video, url, id: params.id } };
+}
