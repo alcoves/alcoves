d9622acbfadc4375c3d1472ad877c56b21f66fbd
Brendan Kennedy <brenwken@gmail.com>
2021-03-29 18:26:01 -0400

    Add admin page to list all videos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import { useState, } from 'react';
 import { useRouter, } from 'next/router';
 import { signOut, useSession, } from 'next-auth/client';
-import { Flex, Spacer, Box, Avatar, Img, Menu, MenuButton, MenuList, MenuItem, } from '@chakra-ui/react';
+import { Button, Flex, Spacer, Box, Avatar, Img, Menu, MenuButton, MenuList, MenuItem, } from '@chakra-ui/react';
+import isAdmin from '../utils/isAdmin';
 
 export default function Navigation() {
   const router = useRouter();
   const [open, setOpen] = useState(false);
-  const [session, loading] = useSession();
+  const [session] = useSession();
 
   return (
     <Flex h='48px' bg='gray.700'>
@@ -23,22 +24,25 @@ export default function Navigation() {
       <Spacer />
       <Box p='1'>
         {session && session.user ?
-          <Menu>
-            <MenuButton h='100%'>
-              <Avatar
-                h='30px' w='30px'
-                name={session.user.name}
-                src={session.user.image}
-                onClick={() => setOpen(!open)}
-              />
-            </MenuButton>
-            <MenuList>
-              <MenuItem onClick={() => router.push(`/u/${session.id}`)}>Profile</MenuItem>
-              <MenuItem onClick={() => router.push('/studio')}>Studio</MenuItem>
-              <MenuItem onClick={() => router.push('/account')}>Account</MenuItem>
-              <MenuItem onClick={signOut}>Log out</MenuItem>
-            </MenuList>
-          </Menu>
+          <Flex justify='center' align='center' h='100%'>
+            <Button me='10px' size='sm' onClick={() => router.push('/studio')}>Studio</Button>
+            <Menu>
+              <MenuButton me='10px'>
+                <Avatar
+                  h='30px' w='30px'
+                  name={session.user.name}
+                  src={session.user.image}
+                  onClick={() => setOpen(!open)}
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={() => router.push(`/u/${session.id}`)}>Profile</MenuItem>
+                <MenuItem onClick={() => router.push('/account')}>Account</MenuItem>
+                {session?.id && isAdmin(session.id) && <MenuItem onClick={() => router.push('/admin')}>Admin</MenuItem>}
+                <MenuItem onClick={signOut}>Log out</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
           :
           <Flex w='full' h='full' align='center'>
             <Avatar
