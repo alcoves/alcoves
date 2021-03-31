33dc898b400ee06f5b0439ce86278705b78ba78d
Brendan Kennedy <brenwken@gmail.com>
2021-03-30 22:31:51 -0400

    🎉 Add video views

--- src/pages/api/videos/[id]/views.js
+++ src/pages/api/videos/[id]/views.js
@@ -4,31 +4,41 @@ import db from '../../../../utils/db';
 export default async function handler(req, res) {
   try {
     if (req.method === 'POST') {
-      console.log('logging video view');
-
+      const videoId = req.query.id;
       const session = await getSession({ req });
       const requestIP = req.headers['cf-connecting-ip'];
       if (!requestIP) return res.status(400).send(`invalid request ip: ${requestIP}`);
 
-      const video = await db.video.findFirst({ where: { videoId: req.query.id } });
+      const video = await db.video.findFirst({ where: { videoId } });
       if (!video) return res.status(404).end();
       if (video.duration <= 0) return res.status(400).end();
 
-      const backdatedTimestamp = Date.now() - (video.duration * 1000);
-      const recentView = await db.view.findFirst({ where: { videoId: req.query.id } });
+      const backdatedTimestamp = new Date(Date.now() - (video.duration * 1000)).toISOString();
+      const recentView = await db.videoView.findMany({
+        where: {
+          videoId,
+          ip: requestIP,
+          createdAt: {
+            gte: backdatedTimestamp,
+          },
+        },
+      });
 
       if (recentView) {
         return res.status(400).send('view too recent, not counted');
       }
 
-      const data = {
-        ip: requestIP,
-        videoId: req.video.id,
-      };
-
-      if (session?.id) data.userId = session.id;
-      await db.view.create({ data });
-      // await db.video.update({  })
+      const newView = { ip: requestIP, videoId };
+      if (session?.id) newView.userId = session.id.toString();
+      await db.videoView.create({ data: newView });
+      await db.video.update({ 
+        where: { videoId },
+        data: {
+          views: {
+            increment: 1,
+          },
+        },
+      });
       return res.status(200).end();
     }
     res.status(400).end();
