fae8b6051eedb81c0a7924b8f374f37c4b47232b
Brendan Kennedy <brenwken@gmail.com>
2021-04-05 22:43:48 -0400

    Remove geopattern

--- src/pages/u/[id].js
+++ src/pages/u/[id].js
@@ -1,19 +1,11 @@
 import useSWR from 'swr';
 import Head from 'next/head';
 import { useRouter, } from 'next/router';
-import geo from 'geopattern';
-import { Box, Avatar, Flex, Image, Heading, } from '@chakra-ui/react';
+import { Box, Avatar, Flex, Heading, } from '@chakra-ui/react';
 import Layout from '../../components/Layout';
 import VideoGrid from '../../components/VideoGrid/Index';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
-
-function countViews(videos) {
-  return videos.reduce((acc, cv) => {
-    acc += cv.views;
-    return acc;
-  }, 0);
-}
 
 function UserProfile({ id }) {
   const { data: user } = useSWR(`/api/users/${id}`, fetcher);
@@ -25,27 +17,19 @@ function UserProfile({ id }) {
         <title>bken.io</title>
       </Head>
       <Layout>
-        <Flex w='full' direction='column'>
-          <Image
-            w='full'
-            h='300px'
-            objectFit='cover'
-            src={geo.generate(user?.id).toDataUri()}
-          />
-          <Flex p='2' justify='center' align='center'>
-            <Flex direction='column' align='center'>
-              <Avatar
-                mt='2'
-                src={user?.image}
-                w='75px' h='75px'
-              />
-              <Heading size='lg'>{user?.name}</Heading>
-            </Flex>
+        <Flex p='2' justify='center' align='center'>
+          <Flex direction='column' align='center'>
+            <Avatar
+              mt='2'
+              src={user?.image}
+              w='75px' h='75px'
+            />
+            <Heading size='lg'>{user?.name}</Heading>
           </Flex>
-          <Box p='2'>
-            {videos && <VideoGrid videos={videos} />}
-          </Box>
         </Flex>
+        <Box p='2'>
+          {videos && <VideoGrid videos={videos} />}
+        </Box>
       </Layout>
     </>
   );
