b264bd0097200a6107c3427b32b3c5a69e1c9c63
Brendan Kennedy <brenwken@gmail.com>
2021-05-09 22:04:14 -0400

    Improve studio video grid column widths for mobile

--- src/components/Studio/StudioVideoGrid.js
+++ src/components/Studio/StudioVideoGrid.js
@@ -3,7 +3,7 @@ import StudioVideo from './StudioVideo';
 
 export default function StudioVideoGrid({ videos }) {
   return ( 
-    <SimpleGrid columns={[2, null, 3]} spacing='10px'>
+    <SimpleGrid columns={[1, 1, 3, 3, 4]} spacing='10px'>
       {videos.map(v => <StudioVideo key={v.videoId} v={v}/>)}
     </SimpleGrid >
   );  
