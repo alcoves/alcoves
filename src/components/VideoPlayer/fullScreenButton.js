bc33b1d603c74c7251869c3d798915de7123b62d
Brendan Kennedy <brenwken@gmail.com>
2021-06-11 17:28:20 -0400

    Disable PIP button on firefox

--- src/components/VideoPlayer/fullScreenButton.js
+++ src/components/VideoPlayer/fullScreenButton.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Box, } from '@chakra-ui/react';
 import { IoExpand, } from 'react-icons/io5';
+import { detect, } from'detect-browser';
+
+const browser = detect();
 
 function FullScreenButton({ vRef }) {
-  return (
-    <Box>
-      <IoExpand
-        size='20px'
-        stroke='white'
-        cursor='pointer'
-        onClick={() => { vRef.current.requestFullscreen(); }}
-      />
-    </Box>
-  );
+  switch (browser && browser.name) {
+  case 'firefox':
+    return <div />;
+  default:
+    return (
+      <Box>
+        <IoExpand
+          size='20px'
+          stroke='white'
+          cursor='pointer'
+          onClick={() => { vRef.current.requestFullscreen(); }}
+        />
+      </Box>
+    );
+  }
 }
 
 export default FullScreenButton;
