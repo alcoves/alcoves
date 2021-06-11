4da66eb336d7228436499006cc2a49cfb8f670f2
Brendan Kennedy <brenwken@gmail.com>
2021-06-11 17:35:42 -0400

    I'm dumb

--- src/components/VideoPlayer/fullScreenButton.js
+++ src/components/VideoPlayer/fullScreenButton.js
@@ -1,29 +1,18 @@
 import React from 'react';
 import { Box, } from '@chakra-ui/react';
 import { IoExpand, } from 'react-icons/io5';
-import { detect, } from'detect-browser';
-
-const browser = detect();
 
 function FullScreenButton({ vRef }) {
-  switch (browser && browser.name) {
-  case 'edge':
-  case 'opera':
-  case 'safari':
-  case 'chrome':
-    return (
-      <Box>
-        <IoExpand
-          size='20px'
-          stroke='white'
-          cursor='pointer'
-          onClick={() => { vRef.current.requestFullscreen(); }}
-        />
-      </Box>
-    );
-  default:
-    return <div />;
-  }
+  return (
+    <Box>
+      <IoExpand
+        size='20px'
+        stroke='white'
+        cursor='pointer'
+        onClick={() => { vRef.current.requestFullscreen(); }}
+      />
+    </Box>
+  );
 }
 
 export default FullScreenButton;
--- src/components/VideoPlayer/pictureInPictureButton.js
+++ src/components/VideoPlayer/pictureInPictureButton.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Box, } from '@chakra-ui/react';
 import { IoOpenOutline, } from 'react-icons/io5';
+import { detect, } from'detect-browser';
+
+const browser = detect();
 
 function PictureInPictureButton({ vRef }) {
-  return (
-    <Box mx='2'>
-      <IoOpenOutline
-        size='20px'  
-        stroke='white'
-        cursor='pointer'
-        onClick={() => { vRef.current.requestPictureInPicture(); }}
-      />
-    </Box>
-  );
+  switch (browser && browser.name) {
+  case 'edge':
+  case 'opera':
+  case 'safari':
+  case 'chrome':
+    return (
+      <Box mx='2'>
+        <IoOpenOutline
+          size='20px'  
+          stroke='white'
+          cursor='pointer'
+          onClick={() => { vRef.current.requestPictureInPicture(); }}
+        />
+      </Box>
+    );
+  default:
+    return <div />;
+  }
 }
 
 export default PictureInPictureButton;
