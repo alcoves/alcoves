bd0666140702ade55d92bc8220a865e610201d0e
Brendan Kennedy <brenwken@gmail.com>
2021-06-15 17:44:47 -0400

    Account page styling

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,61 +1,58 @@
-import { useSession, signOut, } from 'next-auth/client';
-import { IoSunnyOutline, IoSunny, } from 'react-icons/io5';
-import { Flex, IconButton, CircularProgress, useColorMode, } from '@chakra-ui/react';
+import { useSession, } from 'next-auth/client';
+import { Flex, Avatar, useColorMode, VStack, SkeletonCircle, Heading, SkeletonText, Text, Switch, } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 
 export default function Account() {
   const { colorMode, toggleColorMode } = useColorMode();
   const [session, loading] = useSession();
 
-  if (loading) {
-    return (
-      <Layout>
-        <div className='flex w-full justify-center pt-4'>
-          <CircularProgress isIndeterminate />
-        </div>
-      </Layout>
-    );
-  }
-
-  if (session) {
-    return (
-      <Layout>
-        <div className='flex flex-col pt-4'>
-          <div width='500px' align='center' margin='medium'>
-            <img className='h-32 w-32 rounded-full' src={session.user.image} alt='avatar' />
-            <h4 className='mt-2 text-3xl font-bold text-gray-200'>
-              {session.user.name}
-            </h4>
-            <p className='mb-2 text-sm lowercase font-bold text-gray-500'>
-              {session.user.email}
-            </p>
-            <p className='mb-2 text-sm lowercase font-bold text-gray-500'>
-              User ID: {session.id}
-            </p>
-          </div>
-          <Flex cursor='pointer' justifyContent='center'>
-            <IconButton
-              size='sm'
-              mx='5px'
-              variant='ghost'
-              onClick={toggleColorMode}
-              icon={colorMode === 'light' ? <IoSunny size='1.5rem'/> : <IoSunnyOutline size='1.5rem'/>}
-            />
-          </Flex>
-        </div>
-      </Layout>
-    );
-  }
-
   return (
     <Layout>
-      <div className='flex flex-col pt-4'>
-        <div width='500px' align='center' margin='medium'>
-          <div className='text-gray-200 uppercase text-3xl font-extrabold h-8 py-1 px-2 tracking-wide'>
-            Please sign in
-          </div>
-        </div>
-      </div>
+      <Flex justify='center' pt='5' direction='row'>
+        <Flex direction='column' rounded='lg' w='400px' p='5'>
+          <Flex direction='row' justify='center' w='100%' h='75px'>
+            <SkeletonCircle w='60px' h='60px' isLoaded={Boolean(!loading && session?.user?.image)}>
+              <Avatar
+                w='60px' h='60px'
+                name={session?.user?.name}
+                src={session?.user?.image}
+              />
+            </SkeletonCircle>
+            <SkeletonText
+              ml='5' w='100%' noOfLines={3} spacing={3}
+              isLoaded={Boolean(!loading && session?.user?.name)}
+            >
+              <Heading size='sm'>
+                {session?.user?.name}
+              </Heading>
+              <Text fontSize='.85rem'>{session?.user?.email}</Text>
+              <Text fontSize='.85rem'>User ID: {session?.id}</Text>
+            </SkeletonText>
+          </Flex>
+          <VStack w='100%'>
+            <Flex alignItems='center' justifyContent='space-between' w='100%'>
+              <Text>Dark Mode</Text>
+              <Switch size='sm' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+            </Flex>
+            <Flex alignItems='center' justifyContent='space-between' w='100%'>
+              <Text>Videos Uploaded</Text>
+              <Text>N/A</Text>
+            </Flex>
+            <Flex alignItems='center' justifyContent='space-between' w='100%'>
+              <Text>Minutes Uploaded</Text>
+              <Text>N/A</Text>
+            </Flex>
+            <Flex alignItems='center' justifyContent='space-between' w='100%'>
+              <Text>Video Views</Text>
+              <Text>N/A</Text>
+            </Flex>
+            <Flex alignItems='center' justifyContent='space-between' w='100%'>
+              <Text>Data Stored</Text>
+              <Text>N/A</Text>
+            </Flex>
+          </VStack>
+        </Flex>
+      </Flex>
     </Layout>
   );
 }
