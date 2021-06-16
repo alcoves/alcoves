7948056a257b17b8aced45ebbf0e21985a22171b
Brendan Kennedy <brenwken@gmail.com>
2021-06-16 18:16:53 -0400

    Livestreaming test

--- src/components/VideoPlayer/duration.js
+++ src/components/VideoPlayer/duration.js
@@ -1,12 +1,26 @@
-import { Text, } from '@chakra-ui/layout';
+import { Text, Flex, } from '@chakra-ui/react';
 import React, { useEffect, useState, } from 'react';
 import videoDuration from '../../utils/videoDuration';
 
-function Duration({ vRef }) {
+function Duration({ vRef, player = {} }) {
   const [time, setTime] = useState('0:00 / 0:00');
 
   function formatDuration() {
-    return `${videoDuration(vRef.current.currentTime)} / ${videoDuration(vRef.current.duration)}`;
+    if (Number.isFinite(vRef.current.duration)) {
+      return `${videoDuration(vRef.current.currentTime)} / ${videoDuration(vRef.current.duration)}`;
+    }
+
+    return (
+      <Flex>
+        <Text >
+          {videoDuration(vRef.current.currentTime)}
+        </Text>
+        <Text cursor='pointer' onClick={() => player.seek(player.duration())}
+          ml='2' textTransform='uppercase' color='red.500' fontSize='.8rem' fontWeight='800'>
+          Live
+        </Text>
+      </Flex>
+    );
   }
 
   useEffect(() => {
@@ -17,9 +31,9 @@ function Duration({ vRef }) {
   }, [vRef]);
 
   return (
-    <Text ml='4' select='none' color='lightgrey' fontSize='.8rem' fontWeight='800'>
-      {time}
-    </Text>
+    <Flex ml='4' select='none' color='lightgrey' fontSize='.8rem' fontWeight='800'>
+      <Text>{time}</Text>
+    </Flex>
   );
 }
 
