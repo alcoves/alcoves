1c68b576b9d49180092a3efc571c44830347c7d5
Brendan Kennedy <brenwken@gmail.com>
2021-06-22 16:47:28 -0400

    Add propeller ad verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,11 +12,11 @@ export default class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap' rel='stylesheet' />
           <meta name='version' content='%REACT_APP_GIT_SHA%' />
+          <meta name='propeller' content='49f2d20cb1da685e9da995ccfd679789' />
         </Head>
         <body>
           <script src='https://cdn.jsdelivr.net/npm/hls.js@latest'/>
           <script src='https://cdn.dashjs.org/latest/dash.all.min.js'/>
-          <script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' />
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
