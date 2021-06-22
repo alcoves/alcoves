5f52d9259d9318693f0538e01384a23bcb4cceef
Brendan Kennedy <brenwken@gmail.com>
2021-06-22 17:18:01 -0400

    Google

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, } from 'react';
 import useSWR from 'swr';
 import { Box, Heading, } from '@chakra-ui/react';
 import Layout from '../components/Layout';
@@ -9,11 +9,21 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 function index() {
   const { data } = useSWR('/api/videos', fetcher);
 
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
   return (
     <Layout>
       <Box px='2'>
         <Heading py='2'> Latest Videos </Heading>
         <VideoGrid videos={data} />
+        <ins className='adsbygoogle'
+          style={{ display:'block' }}
+          data-ad-client='ca-pub-1017771648826122'
+          data-ad-slot='1395737646'
+          data-ad-format='auto'
+          data-full-width-responsive='true' />
       </Box>
     </Layout>
   );
