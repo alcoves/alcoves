ae67ffc3699556b034e2dc848ba64dee1f007e31
Brendan Kennedy <brenwken@gmail.com>
2021-06-28 15:43:59 -0400

    Fix login page spacing

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,8 +5,8 @@ import { providers as getProviders, signIn, } from 'next-auth/client';
 export default function Login({ providers }) {
   return (
     <Flex justify='center'>
-      <Flex align='center' pt='20%' direction='column'>
-        <Image height='auto' width='200px' src='/favicon.ico' alt='logo' />
+      <Flex align='center' pt='10vh' direction='column'>
+        <Image height='180px' width='200px' src='/favicon.ico' alt='logo' />
         <Heading>Dive into bken</Heading>
         {Object.values(providers).map(provider => (
           <Box key={provider.name} w='full'>
