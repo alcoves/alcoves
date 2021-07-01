d474fa01cda8cdd9eb36f96429952a6828f2539b
Brendan Kennedy <brenwken@gmail.com>
2021-07-01 14:36:39 -0400

    Remove hardcoded url

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function index({ url, videos }) {
 export async function getServerSideProps() {
   const url = 'http://localhost:3000/api/videos';
   const videos = await fetcher(url);
-  return { props: { videos, url: 'https://bken.io/api/videos' } };
+  return { props: { videos, url } };
 }
 
 export default index;
