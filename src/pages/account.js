74b7383572be12151fdd6ffb1ba4c3617312b01b
Brendan Kennedy <brenwken@gmail.com>
2021-07-02 14:03:53 -0400

    Add more loading states

--- src/pages/account.js
+++ src/pages/account.js
@@ -9,7 +9,7 @@ export default function Account() {
   return (
     <Layout>
       <Flex justify='center' pt='5' direction='row'>
-        <Flex direction='column' rounded='lg' p='5'>
+        <Flex direction='column' rounded='lg' p='5' minW='400px'>
           <Flex direction='row' justify='center' w='100%' h='75px'>
             <SkeletonCircle w='60px' h='60px' isLoaded={Boolean(!loading && session?.user?.image)}>
               <Avatar
@@ -29,28 +29,33 @@ export default function Account() {
               <Text fontSize='.85rem'>User ID: {session?.id}</Text>
             </SkeletonText>
           </Flex>
-          <VStack w='100%'>
-            <Flex alignItems='center' justifyContent='space-between' w='100%'>
-              <Text>Dark Mode</Text>
-              <Switch size='sm' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-            </Flex>
-            <Flex alignItems='center' justifyContent='space-between' w='100%'>
-              <Text>Videos Uploaded</Text>
-              <Text>N/A</Text>
-            </Flex>
-            <Flex alignItems='center' justifyContent='space-between' w='100%'>
-              <Text>Minutes Uploaded</Text>
-              <Text>N/A</Text>
-            </Flex>
-            <Flex alignItems='center' justifyContent='space-between' w='100%'>
-              <Text>Video Views</Text>
-              <Text>N/A</Text>
-            </Flex>
-            <Flex alignItems='center' justifyContent='space-between' w='100%'>
-              <Text>Data Stored</Text>
-              <Text>N/A</Text>
-            </Flex>
-          </VStack>
+          <SkeletonText
+            w='100%' noOfLines={3} spacing={3}
+            isLoaded={Boolean(!loading && session?.user?.name)}
+          >
+            <VStack w='100%'>
+              <Flex alignItems='center' justifyContent='space-between' w='100%'>
+                <Text>Dark Mode</Text>
+                <Switch size='sm' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+              </Flex>
+              <Flex alignItems='center' justifyContent='space-between' w='100%'>
+                <Text>Videos Uploaded</Text>
+                <Text>N/A</Text>
+              </Flex>
+              <Flex alignItems='center' justifyContent='space-between' w='100%'>
+                <Text>Minutes Uploaded</Text>
+                <Text>N/A</Text>
+              </Flex>
+              <Flex alignItems='center' justifyContent='space-between' w='100%'>
+                <Text>Video Views</Text>
+                <Text>N/A</Text>
+              </Flex>
+              <Flex alignItems='center' justifyContent='space-between' w='100%'>
+                <Text>Data Stored</Text>
+                <Text>N/A</Text>
+              </Flex>
+            </VStack>
+          </SkeletonText>
         </Flex>
       </Flex>
     </Layout>
