46c09216d6001682db963e3e43473179bcd2e61a
Brendan Kennedy <brenwken@gmail.com>
2021-07-06 02:01:01 -0400

    Fix issue deleting large videos

--- src/utils/s3.js
+++ src/utils/s3.js
@@ -16,4 +16,27 @@ const s3 = new AWS.S3({
   endpoint: process.env.WASABI_ENDPOINT,
 });
 
-module.exports = { s3 };
+async function listObjects(params = {}, key = 'Contents', items = [] ) {
+  const req = await s3.listObjectsV2(params).promise();
+  req[key].map((i)=> items.push(i));
+  if (req.NextContinuationToken) {
+    params.ContinuationToken = req.NextContinuationToken;
+    return listObjects(params, key, items);
+  }
+  return items;
+}
+
+async function deleteFolder(params) {
+  const req = await s3.listObjectsV2(params).promise();
+  await s3.deleteObjects({
+    Bucket: 'cdn.bken.io',
+    Delete: { Objects: req.Contents },
+  }).promise();
+
+  if (req.NextContinuationToken) {
+    params.ContinuationToken = req.NextContinuationToken;
+    return deleteFolder(params);
+  }
+}
+
+module.exports = { s3, listObjects, deleteFolder };
