6cdd6e98cdf0f45660e16a4f7563d70c33d92048
Brendan Kennedy <brenwken@gmail.com>
2021-07-08 17:53:04 -0400

    Fix fullscreen toggle

--- src/components/VideoPlayer/fullScreenButton.js
+++ src/components/VideoPlayer/fullScreenButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { IconButton, } from '@chakra-ui/react';
 import { IoExpand, } from 'react-icons/io5';
 
-function FullScreenButton({ vRef }) {
+function FullScreenButton({ toggle }) {
   return (
     <IconButton variant='ghost' size='sm'>
       <IoExpand
         size='20px'
         color='white'
         cursor='pointer'
-        onClick={() => { vRef.current.requestFullscreen(); }}
+        onClick={() => toggle()}
       />
     </IconButton>
   );
