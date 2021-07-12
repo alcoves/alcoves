05275d72906ec864a72b52e0f0c55acf28eb21cb
Brendan Kennedy <brenwken@gmail.com>
2021-07-11 20:53:14 -0400

    Revert some theme changes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,13 @@
 import { extendTheme, } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  components: {
-    Button: {
-      baseStyle: {
-        fontWeight: '800',
-        textTransform: 'uppercase',
-      },
-      sizes: {
-        xs: { fontSize: 'xs' },
-        sm: { fontSize: 'xs' },
-        md: { fontSize: 'sm' },
-        lg: { fontSize: 'md' },
-        xl: { fontSize: 'md' },
-      },
-    },
-  },
+  // components: {
+  //   Button: {
+  //     baseStyle: {
+  //       fontWeight: '800',
+  //     },
+  //   },
+  // },
   brand: {
     900: '#1a365d',
     800: '#153e75',
