49076d80aa34ee97800855420d5e7ac154f4be71
Brendan Kennedy <brenwken@gmail.com>
2021-07-13 01:14:06 -0400

    Place upload state at the top level

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import '../styles/index.css';
-import React, { useEffect, } from 'react';
+import React, { useEffect, useReducer, } from 'react';
 import Head from 'next/head';
 import { Provider, } from 'next-auth/client';
 import { useRouter, } from 'next/router';
 import * as gtag from '../utils/gtag';
 import { Chakra, } from '../styles/chakra';
+import { UploadContext, } from '../context/UploadContext';
+import UploadReducer from '../reducers/UploadReducer';
 
 function App({ Component, pageProps }) {
   const router = useRouter();
+  const [uploads, dispatch] = useReducer(UploadReducer, []);
   
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -24,11 +27,13 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'/>
       </Head>
-      <Provider session={pageProps.session}>
-        <Chakra cookies={pageProps.cookies}>
-          <Component {...pageProps} />
-        </Chakra>
-      </Provider>
+      <UploadContext.Provider value={{ uploads, dispatch }}>
+        <Provider session={pageProps.session}>
+          <Chakra cookies={pageProps.cookies}>
+            <Component {...pageProps} />
+          </Chakra>
+        </Provider>
+      </UploadContext.Provider>
     </>
   );
 }
