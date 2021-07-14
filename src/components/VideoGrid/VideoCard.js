919a700a473c5cc3a3b5e22a8e67ce42ee5b904f
Brendan Kennedy <brenwken@gmail.com>
2021-07-14 17:27:38 -0400

    Cleanup card layout

--- src/components/VideoGrid/VideoCard.js
+++ src/components/VideoGrid/VideoCard.js
@@ -1,14 +1,15 @@
-import { Box, Flex , Text, Spacer,  Skeleton, } from '@chakra-ui/react';
+import { Box, Text, Skeleton, } from '@chakra-ui/react';
 import Link from 'next/link';
 import videoDuration from '../../utils/videoDuration';
 import VideoMeta from './VideoMeta';
 
 export default function VideoCard({ v }) {
   return (
-    <Box borderWidth='1px' borderRadius='md' overflow='hidden'>
+    <Box>
       <Skeleton isLoaded={Boolean(v.videoId)}>
         <Link href={`/v/${v.videoId}`}>
           <Box
+            borderRadius='md'
             boxShadow='inner'
             w='100%' h='200px'
             bgSize='cover' bgColor='black'
@@ -16,27 +17,21 @@ export default function VideoCard({ v }) {
             cursor='pointer' position='relative'
             bgPosition='center' bgRepeat='no-repeat'
           >
-            <Flex w='100%' h='100%' direction='column' p='2'>
-              <Flex>
-                <Spacer/>
-              </Flex>
-              <Spacer/>
-              <Flex align='center' direction='column'>
-                <Spacer/>
-              </Flex>
-              <Spacer/>
-              <Flex>
-                <Spacer/>
-                <Flex bg='rgba(10, 10, 10, .4)' borderRadius='md' px='1' justify='center' align='center'>
-                  <Text color='gray.100' fontSize='xs' fontWeight='bold'>{videoDuration(v.duration)}</Text>
-                </Flex>
-              </Flex>
-            </Flex>        
+            <Text
+              px='1'
+              right={1}
+              bottom={1}
+              align='center'
+              justify='center'
+              borderRadius='md'
+              position='absolute'
+              bg='rgba(10, 10, 10, .4)'
+              color='gray.100' fontSize='xs' fontWeight='bold'>
+              {videoDuration(v.duration)}
+            </Text>
           </Box>
         </Link>
-        <Box p='2' bg='transparent' w='100%'>
-          <VideoMeta v={v}/>
-        </Box>
+        <VideoMeta v={v}/>
       </Skeleton>
     </Box>
   );
