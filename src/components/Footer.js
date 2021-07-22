10cb91e466a9676e245d894a9561a81b58a49605
Brendan Kennedy <brenwken@gmail.com>
2021-07-21 22:07:07 -0400

    Add discord link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,54 @@
-import { Flex, Link, } from '@chakra-ui/react';
 import React from 'react';
+import { IoLogoDiscord, } from 'react-icons/io5';
+import { Flex, HStack, Link, Text, useColorMode, } from '@chakra-ui/react';
+
+const links = {
+  github: `https://github.com/bkenio/reef/commit/${process.env.NEXT_PUBLIC_GIT_SHA}`,
+  discord: 'https://discord.gg/CYAWQKbbVd',
+  statusPage: 'https://bken.statuspage.io',
+};
 
 export default function Footer() {
-  const gitSha = process.env.REACT_APP_GIT_SHA;
-  const webLink = `https://github.com/bken-io/web/commit/${gitSha}`;
-  const message = `Version: ${gitSha}`;
+  const { colorMode } = useColorMode();
+
+  const colors = {
+    bgColor: colorMode === 'dark' ? 'gray.900' : 'gray.100',
+    text1: colorMode === 'dark' ? 'gray.400' : 'gray.700',
+    text2: colorMode === 'dark' ? 'gray.500' : 'gray.800',
+  };
+
   return (
-    <Flex h='48px' w='100%' justify='center' align='center'>
-      {gitSha && <Link fontSize='xs' fontWeight='bold' href={webLink}>{message}</Link>}
+    <Flex h='48px' w='100%' px='5' justify='space-between' align='center' bg={colors.bgColor}>
+      <Flex direction='column'>
+        <Text fontSize='.7rem' color={colors.text1}>
+          {`© ${new Date().getFullYear()} Bken, Inc.`}
+        </Text>
+        <Text fontSize='.6rem' color={colors.text1}>
+          <Link href={links.github}>
+            {process.env.NEXT_PUBLIC_GIT_SHA}
+          </Link>
+        </Text>
+      </Flex>
+
+      <Flex>
+        <HStack>
+          <Link href={links.discord}>
+            <Flex justify='center' align='center'>
+              <IoLogoDiscord size='20'/>
+            </Flex>
+          </Link>
+        </HStack>
+      </Flex>
+
+      <Flex>
+        <HStack>
+          <Text fontSize='.7rem' color={colors.text1}>
+            <Link href={links.statusPage}>
+              Status
+            </Link>
+          </Text>
+        </HStack>
+      </Flex>
     </Flex>
   );
 }
