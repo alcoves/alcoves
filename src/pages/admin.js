9584d295495ba459d743a9a1b5e9e58ee6d758cc
Brendan Kennedy <brenwken@gmail.com>
2021-08-19 00:50:30 -0400

    Some admin stuff

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -12,12 +12,14 @@ import {
   Button,
   Flex,
   Box,
+  HStack,
   Text,
 } from '@chakra-ui/react';
 import moment from 'moment';
 import { useSession, } from 'next-auth/client';
 import axios from 'axios';
 import Layout from '../components/Layout';
+import videoDuration from '../utils/videoDuration';
 
 async function reprocessThumbnail(id) {
   await axios.post('https://bk-det1.bken.dev/tidal/jobs/thumbnail', {
@@ -84,22 +86,50 @@ export default function Admin() {
           {videos.map((v) => (
             <Tr key={v.id}>
               <Td>
-                <Image height='50px' src={v.thumbnail} alt='image'/>
-                <Box>{v.duration}</Box>
-                <Box>{v.mpdLink}</Box>
+                <Box
+                  rounded='md'
+                  h='50px'
+                  position='relative'
+                  bgImage={v.thumbnail}
+                  bgSize='cover'
+                  bgColor='black'
+                  bgPosition='center'
+                  bgRepeat='no-repeat'
+                >
+                  <Flex
+                    position='absolute'
+                    right='0'
+                    bottom='0'
+                    justify='center'
+                    align='center'
+                    bg='rgba(10, 10, 10, .4)'
+                    borderRadius='md'
+                    px='1'
+                  >
+                    <Text color='gray.100' fontSize='xs' fontWeight='bold'>
+                      {videoDuration(v.duration)}
+                    </Text>
+                  </Flex>
+                </Box>
               </Td>
               <Td><Link href={`/v/${v.id}`}>{v.id}</Link></Td>
-              <Td>{v.userId}</Td>
               <Td>
-                <Flex direction='column'>
-                  {v.status}
+                {v.user.id}
+                {v.user.name}
+                {v.user.image}
+              </Td>
+              <Td>
+                <HStack direction='row' align='center'>
+                  <Flex>
+                    {v.status}
+                  </Flex>
                   <Button id={v.id} onClick={() => reprocessVideo(v.id)} my='2' size='xs'>
                     Reprocess Video
                   </Button>
                   <Button id={v.id} onClick={() => reprocessThumbnail(v.id)} my='2' size='xs'>
                     Reprocess Thumbnail
                   </Button>
-                </Flex>
+                </HStack>
               </Td>
               <Td isNumeric>{v.percentCompleted}</Td>
               <Td>{moment(v.createdAt).fromNow()}</Td>
