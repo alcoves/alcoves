00aedbc94dac0acbf7501a41947cd0d6772bdb1c
Brendan Kennedy <brenwken@gmail.com>
2021-08-30 23:03:10 -0400

    Change thumbnail

--- src/pages/api/videos/[id]/thumbnail.js
+++ src/pages/api/videos/[id]/thumbnail.js
@@ -1,12 +1,12 @@
 import { getSession, } from 'next-auth/client';
-import { processThumbnail, } from '../../../../utils/tidal';
+import { createThumbnail, } from '../../../../utils/tidal';
 
 export default async function handler(req, res) {
   try {
     const session = await getSession({ req });
     if (!session) return res.status(403).end();
     if (req.method === 'POST') {
-      await processThumbnail(req.query.id);
+      await createThumbnail(req.query.id);
     }
     res.status(200).end();
   } catch (error) {
