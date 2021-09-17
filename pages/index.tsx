9468ef018abf9aa69acf05236e53097b46549160
Brendan Kennedy <brenwken@gmail.com>
2021-09-17 13:55:34 -0400

    Adding some types

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { Heading } from '@chakra-ui/layout'
 import React from 'react'
 import Layout from '../components/Layout'
 
-export default function Index() {
+export default function Index(): JSX.Element {
   return (
     <Layout>
       <Heading> Ahoy! </Heading>
