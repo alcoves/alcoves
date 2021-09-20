d9d832bed9b71b065ca8641331fdced7b0f18ab1
Brendan Kennedy <brenwken@gmail.com>
2021-09-19 21:58:44 -0400

    Upload videos with signed put url

--- pages/pods/[id].tsx
+++ pages/pods/[id].tsx
@@ -7,6 +7,7 @@ import { fetcher } from '../../utils/fetcher'
 import { Spinner, Flex, Heading, HStack, Text } from '@chakra-ui/react'
 import { DeletePod } from '../../components/Pods/DeletePod'
 import VideoGrid from '../../components/VideoGrid/index'
+import { Upload } from '../../components/Pods/Upload'
 
 export default function Pod(): JSX.Element {
   const router = useRouter()
@@ -27,6 +28,7 @@ export default function Pod(): JSX.Element {
         <Flex direction='column'>
           <Heading>{pod.data.name}</Heading>
           <Text>{pod.data._id}</Text>
+          <Upload podId={pod.data._id} />
           <HStack>
             <DeletePod id={pod.data._id} />
           </HStack>
