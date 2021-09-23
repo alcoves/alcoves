e2cee0bf24678fea7dedd3f7e911d196117663d6
Brendan Kennedy <brenwken@gmail.com>
2021-09-23 01:57:42 -0400

    Tidal updates

--- components/VideoGrid/index.tsx
+++ components/VideoGrid/index.tsx
@@ -1,20 +1,11 @@
-import useSWR from 'swr'
 import VideoCard from './VideoCard'
-import { SimpleGrid } from '@chakra-ui/react'
-import { getApiUrl } from '../../utils/api'
-import { fetcher } from '../../utils/fetcher'
 import { Video } from '../../types'
+import { SimpleGrid } from '@chakra-ui/react'
 
-interface VideoGridProps {
-  podId: string
-}
-
-export default function VideoGrid(props: VideoGridProps): JSX.Element {
-  const { podId } = props
-  const { data: videos } = useSWR(podId ? `${getApiUrl()}/pods/${podId}/videos` : null, fetcher)
+export default function VideoGrid(props: { videos: Video[] }): JSX.Element {
   return (
     <SimpleGrid minChildWidth={[200, 350]} spacing='10px'>
-      {videos?.data?.map((v: Video) => (
+      {props?.videos?.map((v: Video) => (
         <VideoCard key={v._id} v={v} />
       ))}
     </SimpleGrid>
