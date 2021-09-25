beccb2f166504705ae2888f090c61ea4c762b75a
Brendan Kennedy <brenwken@gmail.com>
2021-09-25 14:26:34 -0400

    Don't fail build if env is undefined

--- utils/api.ts
+++ utils/api.ts
@@ -2,12 +2,12 @@ export function getApiUrl(): string {
   if (process.env.NEXT_PUBLIC_BKEN_API_URL) {
     return process.env.NEXT_PUBLIC_BKEN_API_URL
   }
-  throw new Error('NEXT_PUBLIC_BKEN_API_URL is undefined')
+  return ''
 }
 
 export function getTidalUrl(): string {
   if (process.env.NEXT_PUBLIC_TIDAL_API_URL) {
     return process.env.NEXT_PUBLIC_TIDAL_API_URL
   }
-  throw new Error('NEXT_PUBLIC_TIDAL_API_URL is undefined')
+  return ''
 }
