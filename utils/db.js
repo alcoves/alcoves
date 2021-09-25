9c4154c41a2152f27602ce1de42b762148a6e17b
Brendan Kennedy <brenwken@gmail.com>
2021-09-25 14:56:11 -0400

    cert stuff

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,7 @@ import fs from 'fs'
 import { MongoClient } from 'mongodb'
 
 const uri = process.env.MONGODB_URI
-if (process.env.MONGODB_TLS_CA) {
-  fs.writeFileSync('./db.crt', process.env.MONGODB_TLS_CA)
-}
+fs.writeFileSync('./db.crt', process.env.MONGODB_TLS_CA)
 
 const options = {
   tls: true,
@@ -16,10 +14,6 @@ const options = {
 
 let client
 let clientPromise
-
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
