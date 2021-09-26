0a7d7e3c0ebc01eb1c280ff96eab45f4f3c2090f
Brendan Kennedy <brenwken@gmail.com>
2021-09-25 20:56:10 -0400

    Deconstruct env variables correctly

--- utils/api.ts
+++ utils/api.ts
@@ -1,13 +1,7 @@
 export function getApiUrl(): string {
-  if (process.env.NEXT_PUBLIC_BKEN_API_URL) {
-    return process.env.NEXT_PUBLIC_BKEN_API_URL
-  }
-  return ''
+  return process.env.NEXT_PUBLIC_BKEN_API_URL || 'https://api.bken.io'
 }
 
 export function getTidalUrl(): string {
-  if (process.env.NEXT_PUBLIC_TIDAL_API_URL) {
-    return process.env.NEXT_PUBLIC_TIDAL_API_URL
-  }
-  return ''
+  return process.env.NEXT_PUBLIC_TIDAL_API_URL || 'https://tidal.bken.io'
 }
