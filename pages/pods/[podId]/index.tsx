388bde86c0db6041c0cddc6a831fec1ab8f95454
Brendan Kennedy <brenwken@gmail.com>
2021-09-27 01:19:36 -0400

    Add edit name field to pod screen

--- pages/pods/[podId]/index.tsx
+++ pages/pods/[podId]/index.tsx
@@ -2,13 +2,14 @@ import useSWR from 'swr'
 import Layout from '../../../components/Layout'
 
 import { getApiUrl } from '../../../utils/api'
-import { fetcher } from '../../../utils/fetcher'
-import { Spinner, Flex, Heading, HStack, Avatar } from '@chakra-ui/react'
+import { fetcher, fetchMutate } from '../../../utils/fetcher'
+import { Spinner, Flex, Input, Avatar, HStack } from '@chakra-ui/react'
 import { DeletePod } from '../../../components/Pods/DeletePod'
 import VideoGrid from '../../../components/VideoGrid/index'
 import { Upload } from '../../../components/Pods/Upload'
 import { Pod, Video } from '../../../types'
 import { GetServerSidePropsContext } from 'next'
+import { ChangeEvent } from 'react'
 
 interface Props {
   pod: Pod
@@ -18,14 +19,28 @@ interface Props {
   videoRefreshInterval: number
 }
 
+let timer: NodeJS.Timeout
+
 export default function PodView(props: Props): JSX.Element {
-  const { data: pod } = useSWR(props.podFetchUrl, fetcher, {
+  const { data: pod, mutate: mutatePod } = useSWR(props.podFetchUrl, fetcher, {
     fallbackData: props.pod,
   })
   const { data: videos } = useSWR(props.videoFetchUrl, fetcher, {
     fallbackData: props.videos,
     refreshInterval: props.videoRefreshInterval,
   })
+
+  function handlePodNameChange(e: ChangeEvent<HTMLInputElement>) {
+    clearTimeout(timer)
+    timer = setTimeout(async () => {
+      await fetchMutate({
+        method: 'patch',
+        data: { name: e.target.value },
+        url: props.podFetchUrl,
+      })
+      mutatePod()
+    }, 750)
+  }
 
   if (!pod?.data) {
     return (
@@ -41,7 +56,15 @@ export default function PodView(props: Props): JSX.Element {
         <Flex direction='column'>
           <Flex direction='column' align='center' pb='4'>
             <Avatar name={pod.data.name} size='xl' mb='4' />
-            <Heading>{pod.data.name}</Heading>
+            <Input
+              my='2'
+              maxW='300px'
+              size='sm'
+              rounded='md'
+              variant='filled'
+              defaultValue={pod.data.name}
+              onChange={handlePodNameChange}
+            />
             <Upload podId={pod.data._id} />
             <HStack py='2'>
               <DeletePod id={pod.data._id} />
