b3602bf44a5360e4cfe59deea3ae60cbdd70d531
Brendan Kennedy <brenwken@gmail.com>
2021-09-26 22:00:45 -0400

    Multipart uploads

--- components/Pods/Upload.tsx
+++ components/Pods/Upload.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import chunkFile from '../../utils/chunkFile'
 import { useSWRConfig } from 'swr'
 import { getApiUrl } from '../../utils/api'
 import { useDropzone } from 'react-dropzone'
@@ -6,40 +7,66 @@ import { fetchMutate } from '../../utils/fetcher'
 import { Text, Box, Progress } from '@chakra-ui/react'
 import { useCallback, useEffect, useState } from 'react'
 
+interface MultipartResponse {
+  ETag: string
+  PartNumber: number
+}
+
 function RenderUpload(props: { file: File; podId: string }) {
   const { mutate } = useSWRConfig()
   const { file, podId } = props
-  const [progress, setProgress] = useState(0)
   const [started, setStarted] = useState(false)
+  const [completed, setCompleted] = useState(false)
+  const [bytesUploaded, setBytesUploaded] = useState(0)
 
   async function startUpload() {
     try {
       setStarted(true)
       console.log('Begin Video Upload', file)
 
-      const { data } = await fetchMutate({
-        method: 'get',
+      const chunks = chunkFile(file)
+
+      const {
+        data: { uploadId, key, _id, urls },
+      } = await fetchMutate({
+        method: 'post',
         url: `${getApiUrl()}/pods/${podId}/videos/upload`,
+        data: { chunks: chunks.length },
       })
 
-      console.log('Signed URL fetched', data)
-      await axios.put(data.url, file, {
-        onUploadProgress: e => {
-          setProgress((e.loaded / e.total) * 100)
-        },
+      const results = await Promise.all(
+        chunks.map((chunk, i) => {
+          console.log(`uploading part ${i} to ${urls[i]}`)
+          return axios.put(urls[i], chunk, {
+            onUploadProgress: e => {
+              setBytesUploaded(p => p + e.loaded)
+            },
+          })
+        })
+      )
+
+      const parts: MultipartResponse[] = []
+      results.forEach(({ headers }, i) => {
+        parts.push({ ETag: headers.etag, PartNumber: i + 1 })
       })
 
-      console.log('Upload to S3 complete')
+      console.log('Multipart Upload Complete')
       await fetchMutate({
         method: 'post',
-        data: { title: file.name },
-        url: `${getApiUrl()}/pods/${podId}/videos/${data._id}`,
+        data: {
+          key,
+          parts,
+          uploadId,
+          title: file.name,
+        },
+        url: `${getApiUrl()}/pods/${podId}/videos/${_id}`,
       })
     } catch (error) {
       console.error(error)
     } finally {
       mutate(`getApiUrl()}/pods/${podId}/videos/`)
-      setProgress(0)
+      setBytesUploaded(0)
+      setCompleted(true)
     }
   }
 
@@ -49,13 +76,19 @@ function RenderUpload(props: { file: File; podId: string }) {
     }
   }, [file, podId, started])
 
-  if (progress) {
+  if (bytesUploaded || completed || !started) {
     return (
       <Box py='2' w='200px'>
         <Text fontSize='xs' isTruncated>
           {file.name}
         </Text>
-        <Progress value={progress} colorScheme='green' size='xs' />
+        <Progress
+          hasStripe
+          size='xs'
+          isAnimated
+          colorScheme='green'
+          value={(bytesUploaded / file.size) * 100}
+        />
       </Box>
     )
   }
