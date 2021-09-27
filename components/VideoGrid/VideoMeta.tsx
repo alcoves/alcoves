279612d0e7c84c7f1f448d86ea0452c0c9321f01
Brendan Kennedy <brenwken@gmail.com>
2021-09-27 00:50:19 -0400

    Add title editing

--- components/VideoGrid/VideoMeta.tsx
+++ components/VideoGrid/VideoMeta.tsx
@@ -1,44 +1,95 @@
-import { Flex, Text, Avatar, Heading, HStack } from '@chakra-ui/react'
 import moment from 'moment'
-import abbreviateNumber from '../../utils/abbreviateNumber'
 import Link from 'next/link'
-import { Video } from '../../types'
 import DeleteVideo from './DeleteVideo'
+import abbreviateNumber from '../../utils/abbreviateNumber'
+import { Video } from '../../types'
+import { useSession } from 'next-auth/react'
+import {
+  Input,
+  Flex,
+  Text,
+  Fade,
+  Avatar,
+  Heading,
+  HStack,
+  VStack,
+  Spinner,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react'
+import { ChangeEvent, useState } from 'react'
+import { fetchMutate } from '../../utils/fetcher'
+import { getApiUrl } from '../../utils/api'
+
+let timer: NodeJS.Timeout
 
 export default function VideoMeta(props: { v: Video }): JSX.Element {
   const { v } = props
+  const { data: session } = useSession()
+  const [saving, setSaving] = useState(false)
   const createdAt = moment(v.createdAt).fromNow()
   const metadata = `${abbreviateNumber(v.views)} views - ${createdAt}`
 
+  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
+    clearTimeout(timer)
+    timer = setTimeout(async () => {
+      setSaving(true)
+      await fetchMutate({
+        method: 'patch',
+        data: { title: e.target.value },
+        url: `${getApiUrl()}/videos/${v._id}`,
+      })
+      setSaving(false)
+    }, 750)
+  }
+
   return (
-    <HStack spacing='12px' py='2' align='start' minH='75px'>
-      {v?.owner && (
-        <Flex>
-          <Link passHref href={`/u/${v.owner._id}`}>
-            <Avatar
-              h='40px'
-              w='40px'
-              cursor='pointer'
-              src={v?.owner?.image}
-              name={v?.owner?.name[0]}
-            />
-          </Link>
-        </Flex>
-      )}
-      <Flex direction='column' overflow='hidden'>
+    <VStack spacing={2} mt='2' align='start'>
+      {v?.owner?._id === session?.id ? (
+        <InputGroup>
+          <Input
+            w='100%'
+            size='sm'
+            rounded='md'
+            variant='filled'
+            defaultValue={v.title}
+            onChange={handleTitleChange}
+          />
+          <InputRightElement color='green.500'>
+            <Fade in={saving}>
+              <Spinner size='xs' />
+            </Fade>
+          </InputRightElement>
+        </InputGroup>
+      ) : (
         <Link passHref href={`/v/${v._id}`}>
           <Heading noOfLines={2} cursor='pointer' size='sm'>
             {v.title}
           </Heading>
         </Link>
-        <Link passHref href={`/u/${v.owner._id}`}>
-          <Text cursor='pointer' fontSize='xs'>
-            {v?.owner?.name}
-          </Text>
-        </Link>
-        <Text fontSize='xs'>{metadata}</Text>
-      </Flex>
-      <DeleteVideo id={v._id} />
-    </HStack>
+      )}
+      <HStack spacing='12px' justify='space-between' w='100%'>
+        <Flex>
+          <Link passHref href={`/u/${v.owner._id}`}>
+            <Avatar
+              h='35px'
+              w='35px'
+              cursor='pointer'
+              src={v?.owner?.image}
+              name={v?.owner?.name[0]}
+            />
+          </Link>
+          <Flex pl='2' direction='column' overflow='hidden'>
+            <Link passHref href={`/u/${v.owner._id}`}>
+              <Text cursor='pointer' fontSize='xs'>
+                {v?.owner?.name}
+              </Text>
+            </Link>
+            <Text fontSize='xs'>{metadata}</Text>
+          </Flex>
+        </Flex>
+        <DeleteVideo id={v._id} />
+      </HStack>
+    </VStack>
   )
 }
