f0bfbf29412e035d18fef6da7720fc19a4164e68
Brendan Kennedy <brenwken@gmail.com>
2021-09-26 20:13:39 -0400

    Multi file upload

--- components/Pods/Upload.tsx
+++ components/Pods/Upload.tsx
@@ -1,57 +1,77 @@
 import axios from 'axios'
-import { Box, Progress } from '@chakra-ui/react'
-import { useDropzone } from 'react-dropzone'
-import { getApiUrl } from '../../utils/api'
-import { fetchMutate } from '../../utils/fetcher'
-import { useCallback, useEffect, useState } from 'react'
 import { useSWRConfig } from 'swr'
+import { getApiUrl } from '../../utils/api'
+import { useDropzone } from 'react-dropzone'
+import { fetchMutate } from '../../utils/fetcher'
+import { Text, Box, Progress } from '@chakra-ui/react'
+import { useCallback, useEffect, useState } from 'react'
 
-export function Upload(props: { podId: string }): JSX.Element {
+function RenderUpload(props: { file: File; podId: string }) {
   const { mutate } = useSWRConfig()
-  const { podId } = props
+  const { file, podId } = props
   const [progress, setProgress] = useState(0)
-  const [upload, setUpload] = useState<File | null>(null)
-  const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
-    setUpload(acceptedFiles[0])
-  }, [])
-  const { getRootProps, getInputProps } = useDropzone({ onDrop })
+  const [started, setStarted] = useState(false)
 
-  async function uploadFile(file: File) {
+  async function startUpload() {
     try {
+      setStarted(true)
       console.log('Begin Video Upload', file)
-      // Create video returns a url to begin the upload
+
       const { data } = await fetchMutate({
         method: 'get',
         url: `${getApiUrl()}/pods/${podId}/videos/upload`,
       })
 
-      // Upload file
       console.log('Signed URL fetched', data)
-      await axios.put(data.url, upload, {
+      await axios.put(data.url, file, {
         onUploadProgress: e => {
           setProgress((e.loaded / e.total) * 100)
         },
-      }) // Uploads to s3
+      })
+
       console.log('Upload to S3 complete')
       await fetchMutate({
         method: 'post',
         data: { title: file.name },
         url: `${getApiUrl()}/pods/${podId}/videos/${data._id}`,
-      }) // Enqueues jobs
-      console.log('Jobs successfully enqueued')
+      })
     } catch (error) {
       console.error(error)
     } finally {
-      mutate(`${getApiUrl()}/pods`)
+      mutate(`getApiUrl()}/pods/${podId}/videos/`)
       setProgress(0)
-      setUpload(null)
     }
   }
 
   useEffect(() => {
-    if (upload) uploadFile(upload)
-  }, [upload])
+    if (file && !started) {
+      startUpload()
+    }
+  }, [file, podId, started])
+
+  if (progress) {
+    return (
+      <Box py='2' w='200px'>
+        <Text fontSize='xs' isTruncated>
+          {file.name}
+        </Text>
+        <Progress value={progress} colorScheme='green' size='xs' />
+      </Box>
+    )
+  }
+
+  return null
+}
+
+export function Upload(props: { podId: string }): JSX.Element {
+  const { podId } = props
+  const [uploads, setUploads] = useState<File[] | []>([])
+
+  const onDrop = useCallback(acceptedFiles => {
+    acceptedFiles.map((f: File) => setUploads((p: File[]) => [...p, f]))
+  }, [])
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop })
 
   return (
     <>
@@ -64,13 +84,12 @@ export function Upload(props: { podId: string }): JSX.Element {
         cursor='pointer'
         p='4px 10px 4px 10px'
       >
-        <input {...getInputProps()} disabled={Boolean(progress)} />
-        {progress ? (
-          <Progress value={progress} w='200px' colorScheme='green' size='xs' />
-        ) : (
-          'Upload Here'
-        )}
+        <input {...getInputProps()} multiple />
+        {'Upload Here'}
       </Box>
+      {uploads?.map((file: File, index) => {
+        return <RenderUpload key={index} file={file} podId={podId} />
+      })}
     </>
   )
 }
