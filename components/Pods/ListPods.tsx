0408a5710aa6cb05828b716442cc59388362129a
Brendan Kennedy <brenwken@gmail.com>
2021-09-27 23:00:22 -0400

    Fix list pods link

--- components/Pods/ListPods.tsx
+++ components/Pods/ListPods.tsx
@@ -1,24 +1,30 @@
 import useSWR from 'swr'
-import Link from 'next/link'
 import { fetcher } from '../../utils/fetcher'
-import { Avatar, HStack, Tooltip } from '@chakra-ui/react'
+import { Box, Avatar, HStack, Tooltip } from '@chakra-ui/react'
 import { getApiUrl } from '../../utils/api'
 import CreatePod from './CreatePod'
+import { useRouter } from 'next/router'
 
 const fetchUrl = `${getApiUrl()}/pods`
 
 export default function ListPods(): JSX.Element {
+  const router = useRouter()
   const { data } = useSWR(fetchUrl, fetcher)
   return (
     <>
       <HStack h='100%' align='center'>
         {data?.data?.map((p: { _id: string; name: string }) => {
           return (
-            <Link key={p._id} href={`/pods/${p._id}`} passHref>
-              <Tooltip label={p.name}>
-                <Avatar name={p.name} size='sm' cursor='pointer' />
-              </Tooltip>
-            </Link>
+            <Tooltip key={p._id} label={p.name}>
+              <Avatar
+                size='sm'
+                name={p.name}
+                cursor='pointer'
+                onClick={() => {
+                  router.push(`/pods/${p._id}`)
+                }}
+              />
+            </Tooltip>
           )
         })}
         <CreatePod />
