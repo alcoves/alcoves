02451d994b71a9b73c86a31fa8d569767079d529
Brendan Kennedy <brenwken@gmail.com>
2021-09-28 21:56:36 -0400

    Styling

--- styles/chakra.js
+++ styles/chakra.js
@@ -1,5 +1,5 @@
-import { ChakraProvider, cookieStorageManager, localStorageManager } from '@chakra-ui/react'
 import theme from './theme'
+import { ChakraProvider, cookieStorageManager, localStorageManager } from '@chakra-ui/react'
 
 export function Chakra({ cookies, children }) {
   const colorModeManager =
