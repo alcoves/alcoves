843aa241c36eb8099c0b9a217658ac6e878134be
Brendan Kennedy <brenwken@gmail.com>
2021-09-29 17:31:49 -0400

    Fixed an issue where player ref could be lost

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,35 +1,43 @@
+// eslint-disable-next-line
+// @ts-nocheck
+
 import 'videojs-persist'
 import 'video.js/dist/video-js.css'
 import 'videojs-hls-quality-selector'
 import 'videojs-contrib-quality-levels'
+import { useEffect, useRef } from 'react'
 import videojs, { VideoJsPlayerOptions } from 'video.js'
-import { useCallback, useEffect, useState } from 'react'
 
 export default function VideoPlayer(props: VideoJsPlayerOptions): JSX.Element {
-  const [videoEl, setVideoEl] = useState(null)
-  const onVideo = useCallback(el => {
-    setVideoEl(el)
-  }, [])
+  const videoRef = useRef(null)
+  const playerRef = useRef(null)
 
   useEffect(() => {
-    if (videoEl == null) return
-    const player = videojs(videoEl, props)
-    // eslint-disable-next-line
-    // @ts-ignore
-    player.hlsQualitySelector({
-      displayCurrentQuality: true,
-    })
-    // eslint-disable-next-line
-    // @ts-ignore
-    player.persist()
-    return () => {
-      player.dispose()
+    // make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current
+      if (!videoElement) return
+      const player = (playerRef.current = videojs(videoElement, props, () => {
+        player.hlsQualitySelector({
+          displayCurrentQuality: true,
+        })
+      }))
     }
-  }, [props, videoEl])
+  }, [props])
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose()
+        playerRef.current = null
+      }
+    }
+  }, [])
 
   return (
     <div data-vjs-player>
-      <video ref={onVideo} className='vjs-bken video-js vjs-big-play-centered' playsInline />
+      <video ref={videoRef} className='vjs-bken video-js vjs-big-play-centered' />
     </div>
   )
 }
