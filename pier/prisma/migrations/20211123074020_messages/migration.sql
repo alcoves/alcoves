e596c3b565c9c396aac9a9d51eb0ddbd2ff76c55
Brendan Kennedy <brenwken@gmail.com>
2021-11-23 02:41:08 -0500

    Add messages schema

---
+++ pier/prisma/migrations/20211123074020_messages/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Channel` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Harbour` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Membership` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `channelId` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `content` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Message` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Channel" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Harbour" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Membership" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Message" ADD COLUMN     "channelId" TEXT NOT NULL,
+ADD COLUMN     "content" TEXT NOT NULL,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
