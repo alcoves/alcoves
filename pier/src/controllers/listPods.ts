c707fab414b7a8a14a5f9af509e081c3dc788261
Brendan Kennedy <brenwken@gmail.com>
2021-12-18 12:52:20 -0500

    update listPods.tsx

--- pier/src/controllers/listPods.ts
+++ pier/src/controllers/listPods.ts
@@ -2,7 +2,9 @@ import db from '../config/db'
 
 export async function listPods(req, res) {
   const memberships = await db.membership.findMany({
-    include: { pod: true },
+    include: {
+      pod: true,
+    },
     where: { userId: req.user.id },
   })
 
