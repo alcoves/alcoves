bdb15f092c7f8675ba95b67d09b7b8ed5ec583e7
rustyguts <9222118+rustyguts@users.noreply.github.com>
2021-12-21 23:18:29 -0500

    Upload work

--- components/UploadList.tsx
+++ components/UploadList.tsx
@@ -1,3 +1,20 @@
+import { Box } from '@chakra-ui/react'
+import { useContext } from 'react'
+import { UploadsContext } from '../contexts/uploads'
+
 export default function UploadList() {
-  return <div>uploads</div>
+  const { uploads } = useContext(UploadsContext)
+
+  return (
+    <Box>
+      {uploads.map(u => {
+        return (
+          <Box key={u.status}>
+            <Box> Here is the upload </Box>
+            <pre>{JSON.stringify(u)}</pre>
+          </Box>
+        )
+      })}
+    </Box>
+  )
 }
