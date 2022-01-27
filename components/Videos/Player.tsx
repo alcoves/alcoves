82dc4abfda756385f8458201d94b5f8758e142c3
Brendan Kennedy <brenwken@gmail.com>
2022-01-26 23:51:15 -0500

    Fix the build

--- components/Videos/Player.tsx
+++ components/Videos/Player.tsx
@@ -1,7 +1,18 @@
 import dynamic from 'next/dynamic'
+import { useRef } from 'react'
 
 const ReactHlsPlayer = dynamic(() => import('react-hls-player'), { ssr: false })
 
 export default function Player({ src }: { src: string }) {
-  return <ReactHlsPlayer autoPlay={true} controls={true} width='100%' height='auto' src={src} />
+  const ref = useRef(null)
+  return (
+    <ReactHlsPlayer
+      playerRef={ref}
+      autoPlay={true}
+      controls={true}
+      width='100%'
+      height='auto'
+      src={src}
+    />
+  )
 }
