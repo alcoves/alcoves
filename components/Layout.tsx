7aee2c27230c168cb968388437355dc72a88dd8d
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2022-02-03 15:24:12 -0500

    Sidebar work

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,17 @@
-import Sidebar from './Sidebar'
-import { Flex } from '@chakra-ui/layout'
+import TopNav from './TopNav'
+import SideNav from './SideNav'
+import { Flex, Box } from '@chakra-ui/layout'
 
 const Layout = (props: { children: React.ReactNode }) => (
-  <Flex w='100vw' h='100vh'>
-    <Sidebar />
-    <Flex w='100%' direction='column' overflowY='auto'>
-      <Flex h='100%'>{props.children}</Flex>
+  <Box overflow='scroll' h='100vh' w='100vw'>
+    <TopNav />
+    <Flex>
+      <SideNav />
+      <Flex bg='gray.100' w='100%' direction='column' overflow='scroll'>
+        <Flex h='100%'>{props.children}</Flex>
+      </Flex>
     </Flex>
-  </Flex>
+  </Box>
 )
 
 export default Layout
