ef8cb767e1470f2931b156ab209bfbee9498a034
Brendan Kennedy <brenwken@gmail.com>
2022-02-04 17:13:09 -0500

    Fix scrolling

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,12 +3,12 @@ import SideNav from './SideNav'
 import { Flex, Box } from '@chakra-ui/layout'
 
 const Layout = (props: { children: React.ReactNode }) => (
-  <Box overflow='scroll' h='100vh' w='100vw'>
+  <Box>
     <TopNav />
     <Flex>
       <SideNav />
-      <Flex bg='gray.100' w='100%' direction='column' overflow='scroll'>
-        <Flex h='100%'>{props.children}</Flex>
+      <Flex w='100%' direction='column' overflowY='scroll'>
+        <Flex h='calc(100vh - 50px)'>{props.children}</Flex>
       </Flex>
     </Flex>
   </Box>
