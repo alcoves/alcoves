f8715129f5b4fe5f81103630ba55cbcf9eb99a5b
Brendan Kennedy <brenwken@gmail.com>
2022-02-17 22:22:00 -0500

    Update tests

--- pier/__tests__/register.test.ts
+++
@@ -1,9 +0,0 @@
-import app from '../src/app'
-import supertest from 'supertest'
-
-describe('Register', () => {
-  it('Registers a user', async () => {
-    const res = await supertest(app).post('/register')
-    expect(res.statusCode).toEqual(200)
-  })
-})
