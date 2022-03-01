5d167208863fb71c541f991f40cbacd3213e832f
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-03-01 00:50:52 -0500

    Some uploader work

---
+++ components/Card.tsx
@@ -0,0 +1,13 @@
+import { Box, useColorMode } from '@chakra-ui/react'
+
+export default function Card({ children }: { children: any }) {
+  const { colorMode } = useColorMode()
+
+  const cardBg = colorMode === 'dark' ? 'gray.700' : 'gray.200'
+
+  return (
+    <Box w='100%' h='100%' rounded='md' bg={cardBg}>
+      {children}
+    </Box>
+  )
+}
