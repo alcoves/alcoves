863912e6d0058082e48b9670303d6fcf7b8f780c
rustyguts <9222118+rustyguts@users.noreply.github.com>
2022-03-04 02:31:42 -0500

    Move upload into modal

--- components/Upload/Upload.tsx
+++ components/Upload/Upload.tsx
@@ -1,11 +1,29 @@
 import UploadVideo from './UploadVideo'
 import { useDropzone } from 'react-dropzone'
 import { useCallback, useState } from 'react'
-import { Box, Flex, Heading, Input, useColorMode, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Heading,
+  Input,
+  useColorMode,
+  VStack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react'
+import { IoCloudUpload } from 'react-icons/io5'
 
 export default function Upload() {
   const { colorMode } = useColorMode()
   const [files, setFiles] = useState([]) as any
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const onDrop = useCallback(acceptedFiles => {
     setFiles((prev: any) => [...prev, ...acceptedFiles])
   }, [])
@@ -13,31 +31,51 @@ export default function Upload() {
   const borderColor = colorMode === 'dark' ? 'gray.700' : 'gray.100'
 
   return (
-    <Flex direction='column' align='center'>
-      <Box w='600px'>
-        <Flex
-          {...getRootProps()}
-          p='4'
-          h='200px'
-          align='center'
-          justify='center'
-          borderWidth='2px'
-          borderStyle='dashed'
-          borderColor={borderColor}
-        >
-          <input {...getInputProps()} />
-          <Heading size='md'> Drop here or select to upload </Heading>
-        </Flex>
-        <Input my='2' placeholder='Import video from URL' />
-        {files.length ? (
-          <VStack direction='column' mt='4'>
-            <Heading size='md'> Uploading Queue </Heading>
-            {files.map((f: any, i: number) => (
-              <UploadVideo key={i} file={f} />
-            ))}
-          </VStack>
-        ) : null}
-      </Box>
-    </Flex>
+    <>
+      <Button size='sm' variant='ghost' leftIcon={<IoCloudUpload size='18px' />} onClick={onOpen}>
+        Upload
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Upload</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Flex direction='column' align='center'>
+              <Box w='100%'>
+                <Flex
+                  {...getRootProps()}
+                  p='4'
+                  h='200px'
+                  align='center'
+                  justify='center'
+                  borderWidth='2px'
+                  borderStyle='dashed'
+                  borderColor={borderColor}
+                >
+                  <input {...getInputProps()} />
+                  <Heading size='md'> Drop here or select to upload </Heading>
+                </Flex>
+                <Input my='2' placeholder='Import video from URL' />
+                {files.length ? (
+                  <VStack direction='column' mt='4'>
+                    <Heading size='md'> Uploading Queue </Heading>
+                    {files.map((f: any, i: number) => (
+                      <UploadVideo key={i} file={f} />
+                    ))}
+                  </VStack>
+                ) : null}
+              </Box>
+            </Flex>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
