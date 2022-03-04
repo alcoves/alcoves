b13eae9c907e6e97e296c9fe73d71b0165fe8830
Brendan Kennedy <brenwken@gmail.com>
2022-03-04 00:52:02 -0500

    Add pages

---
+++ pages/terms.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@chakra-ui/react'
+import Layout from '../components/Layout'
+
+export default function Terms() {
+  return (
+    <Layout>
+      <Box>Terms Page</Box>
+    </Layout>
+  )
+}
