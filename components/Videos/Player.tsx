7012c50c8763e60b183f5968a93b2e6d07643974
Brendan Kennedy <brenwken@gmail.com>
2022-03-07 10:31:44 -0500

    Add og tags

--- components/Videos/Player.tsx
+++ components/Videos/Player.tsx
@@ -6,18 +6,18 @@ import { getHlsUrl, getThumanailUrl } from '../../utils/urls'
 
 export default function Player({ v }: { v: Video }) {
   const vRef = useRef(null)
-  const hlsUrl = `${getHlsUrl(v?.cdnUrl)}`
   const thumbnailUrl = `${getThumanailUrl(v?.cdnUrl)}`
 
   useEffect(() => {
-    const video = vRef?.current
-    const hls = new Hls()
-
-    hls.loadSource(hlsUrl)
-    hls.attachMedia(video)
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      video.play()
-    })
+    const video: HTMLMediaElement | any = vRef?.current
+    if (video) {
+      const hls = new Hls()
+      hls.loadSource(`${getHlsUrl(v?.cdnUrl)}`)
+      hls.attachMedia(video)
+      hls.on(Hls.Events.MANIFEST_PARSED, function () {
+        video.play()
+      })
+    }
   }, [])
 
   return (
