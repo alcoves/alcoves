8744463e5e0d0351f1d904f8a53d15f481d6b4b9
Brendan Kennedy <brenwken@gmail.com>
2022-07-08 14:11:33 -0400

    Update admin reprocessing endpoint

--- pier/src/controllers/admin.ts
+++ pier/src/controllers/admin.ts
@@ -1,6 +1,7 @@
 import db from '../config/db'
-import { archiveBucket, cdnBucket } from '../config/s3'
 import { dispatchJob } from '../service/tidal'
+import { parseFramerate } from '../service/videos'
+import { archiveBucket, cdnBucket } from '../config/s3'
 
 // This endpoint does not use cursors and could get very slow
 // Currently used for the admin UI
@@ -21,14 +22,31 @@ export async function reprocessVideo(req, res) {
     height: 480,
     fit: 'cover',
     assetId: video.id,
-    input: `doco:${archiveBucket}/${video.archivePath}`,
-    output: `doco:${cdnBucket}/v/${video.id}/thumbnail.webp`,
+    input: `s3://${archiveBucket}/${video.archivePath}`,
+    output: `s3://${cdnBucket}/v/${video.id}/thumbnail.webp`,
   })
 
-  await dispatchJob('/videos', {
+  const transcodeRes = await dispatchJob('/videos/transcodes/adaptive', {
     assetId: video.id,
-    output: `doco:${cdnBucket}/v/${video.id}`,
-    input: `doco:${archiveBucket}/${video.archivePath}`,
+    output: `s3://${cdnBucket}/v/${video.id}`,
+    input: `s3://${archiveBucket}/${video.archivePath}`,
+  })
+
+  const width = transcodeRes?.data?.metadata?.video[0]?.width || 0
+  const height = transcodeRes?.data?.metadata?.video[0]?.height || 0
+  const length = parseFloat(transcodeRes?.data?.metadata?.format?.duration || 0)
+  const framerate = parseFramerate(transcodeRes?.data?.metadata?.video[0]?.r_frame_rate || 0)
+
+  await db.video.update({
+    where: { id: video.id },
+    data: {
+      status: 'PROCESSING',
+      width,
+      height,
+      length,
+      framerate,
+      metadata: transcodeRes?.data?.metadata || {},
+    },
   })
 
   return res.sendStatus(200)
