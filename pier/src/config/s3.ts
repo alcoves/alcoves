efa400ca7f8f0c60cc3cbe01666fee21bffafdb6
Brendan Kennedy <brenwken@gmail.com>
2022-07-08 22:39:05 -0400

    Add helper

--- pier/src/config/s3.ts
+++ pier/src/config/s3.ts
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk'
 
 AWS.config.update({
-  region: 'us-east-1',
+  region: 'us-east-2',
   accessKeyId: process.env.SPACES_ACCESS_KEY_ID,
   secretAccessKey: process.env.SPACES_SECRET_ACCESS_KEY,
   maxRetries: 8,
@@ -16,14 +16,6 @@ const s3 = new AWS.S3({
   endpoint: process.env.SPACES_ENDPOINT,
 })
 
-export async function getSignedURL(urlParams: { Bucket: string; Key: string }) {
-  return s3.getSignedUrlPromise('getObject', {
-    Key: urlParams.Key,
-    Bucket: urlParams.Bucket,
-    Expires: 86400 * 7, // 7 days
-  })
-}
-
 export function s3URI(uri: string) {
   const s3UrlRe = /^[sS]3:\/\/(.*?)\/(.*)/
   const match = uri.match(s3UrlRe)
@@ -33,6 +25,14 @@ export function s3URI(uri: string) {
     Bucket: match[1],
     Key: match[2],
   }
+}
+
+export async function getSignedURL(urlParams: { Bucket: string; Key: string }) {
+  return s3.getSignedUrlPromise('getObject', {
+    Key: urlParams.Key,
+    Bucket: urlParams.Bucket,
+    Expires: 86400 * 7, // 7 days
+  })
 }
 
 export async function deleteFolder({ Bucket, Prefix }: { Bucket: string; Prefix: string }) {
