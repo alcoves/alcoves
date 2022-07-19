033315a0c532d064044ec145181089af4cb66e3b
Brendan Kennedy <brenwken@gmail.com>
2022-07-19 16:58:37 -0400

    Add embedded page

---
+++ components/Player/EmbeddedPlayer.tsx
@@ -0,0 +1,32 @@
+import { Box } from '@chakra-ui/react'
+
+import { Video } from '../../types/types'
+
+import VideoFrame from './VideoFrame'
+
+export default function EmbeddedPlayer({ v }: { v: Video }) {
+  const height = `${(v.height / v.width) * 100}vw`
+  const maxWidth = `${(v.width / v.height) * 100}vh`
+
+  return (
+    <Box
+      h={height}
+      maxW={maxWidth}
+      w='100vw'
+      maxH='100vh'
+      margin='auto'
+      position='absolute'
+      top='0'
+      left='0'
+      bottom='0'
+      right='0'
+      left='0'
+      overflow='hidden'
+      lineHeight='0'
+    >
+      <Box position='absolute' top='0px' left='0px' right='0px' bottom='0px'>
+        <VideoFrame v={v} />
+      </Box>
+    </Box>
+  )
+}
