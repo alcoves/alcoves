9c6f7b90c9be1802d581b2afff35763881937f60
Brendan Kennedy <brenwken@gmail.com>
2022-12-14 01:50:30 +0000

    Upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,5 @@
 import Home from '../components/Home'
-import Upload from '../components/Upload'
-import Layout from '../components/Layout'
-import { Flex } from '@chakra-ui/react'
-import { useCallback, useState } from 'react'
-import { useDropzone } from 'react-dropzone'
 
 export default function HomePage() {
-  const [file, setFile] = useState(null)
-
-  const onDrop = useCallback(acceptedFiles => {
-    console.log({ acceptedFiles })
-
-    if (acceptedFiles.length) {
-      setFile(acceptedFiles[0])
-    }
-  }, [])
-  const { getRootProps, getInputProps } = useDropzone({ onDrop })
-
-  if (file) {
-    return (
-      <Layout>
-        <Upload file={file} />
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout>
-      <Flex {...getRootProps()} border='solid red 1px'>
-        <input {...getInputProps()} />
-        <Home />
-      </Flex>
-    </Layout>
-  )
+  return <Home />
 }
