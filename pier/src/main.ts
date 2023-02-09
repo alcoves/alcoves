de47d38ca94783ad02059f0be0e1f371e9a49da0
Brendan Kennedy <brenwken@gmail.com>
2023-02-09 06:55:12 +0000

    Add request logger middleware

--- pier/src/main.ts
+++ pier/src/main.ts
@@ -1,11 +1,17 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from './prisma.service';
+import { useRequestLogging } from './middleware/request-logging';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  useRequestLogging(app);
+  app.enableCors({
+    origin: "http://localhost:3000",
+    credentials: true
+  });
   await app.listen(3001);
 }
 bootstrap();
