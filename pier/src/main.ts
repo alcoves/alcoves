2d276c2fb2960f8ed6dc0e41bb4f8dc49dbb2368
Brendan Kennedy <brenwken@gmail.com>
2023-02-15 05:06:20 +0000

    Add swagger

--- pier/src/main.ts
+++ pier/src/main.ts
@@ -2,18 +2,32 @@ import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from './prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { useRequestLogging } from './middleware/request-logging';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
   app.use(cookieParser())
   useRequestLogging(app);
+
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000", // TODO :: Interpolate this value.
     credentials: true
   });
+
+  const config = new DocumentBuilder()
+  .setTitle('Pier API')
+  .setDescription('The API for Reef')
+  .setVersion('1.0')
+  .addTag('bken')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
