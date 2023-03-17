617468bfda371afa8fd9e8b450f0103599328c2e
Brendan Kennedy <brenwken@gmail.com>
2023-03-17 02:55:32 +0000

    Add react-query

--- components/Users/InviteUser.tsx
+++ components/Users/InviteUser.tsx
@@ -12,10 +12,24 @@ import {
   Input,
   VStack,
 } from '@chakra-ui/react'
+import { useState } from 'react'
 import { IoPersonSharp } from 'react-icons/io5'
 
 export default function InviteUser() {
+  const [error, setError] = useState()
+
   const { isOpen, onOpen, onClose } = useDisclosure()
+
+  async function handleCopy() {
+    try {
+      // Fetch invite link from server /invites
+      // Copy the link to clipboard
+      onClose()
+      // Close the modal and create a toast notification
+    } catch (error) {
+      // Handle the error
+    }
+  }
 
   return (
     <>
@@ -29,15 +43,16 @@ export default function InviteUser() {
           <ModalCloseButton />
           <ModalBody>
             <VStack spacing='2'>
-              <Text>{`Enter a valid email address. We'll send them an email with a link to join.`}</Text>
-              <Input placeholder='email@example.com' />
+              <Text>{`This is your invite link. Only share it with people you trust. They can use this link to create an account. Once they register you must verify them.`}</Text>
+              <Button w='100%' onClick={handleCopy}>
+                Copy Invite Link
+              </Button>
             </VStack>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme='gray' mr={3} onClick={onClose}>
-              Cancel
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
             </Button>
-            <Button colorScheme='blue'>Invite</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,27 @@ import '../config/global.css'
 import theme from '../config/theme'
 import DevelopmentCSS from '../components/DevelopmentCSS'
 
+import { useState } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { UserProvider } from '../contexts/UserContext'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App({ Component, pageProps }) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
-    <UserProvider>
-      <ChakraProvider theme={theme}>
-        <DevelopmentCSS />
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Hydrate state={pageProps.dehydratedState}>
+        <UserProvider>
+          <ChakraProvider theme={theme}>
+            <DevelopmentCSS />
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </UserProvider>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 
