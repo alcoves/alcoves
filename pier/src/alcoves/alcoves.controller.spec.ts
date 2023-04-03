9513cc79b7bde936e589c607969b7a540732686e
Brendan Kennedy <brenwken@gmail.com>
2023-04-03 03:44:22 +0000

    Alcoves unit tests

--- pier/src/alcoves/alcoves.controller.spec.ts
+++ pier/src/alcoves/alcoves.controller.spec.ts
@@ -1,20 +1,91 @@
+import { Alcove } from '@prisma/client';
+import { AlcovesService } from './alcoves.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlcovesController } from './alcoves.controller';
-import { AlcovesService } from './alcoves.service';
 
 describe('AlcovesController', () => {
   let controller: AlcovesController;
+
+  const mockAlcovesService = {
+    create: jest.fn(async (alcove: Alcove) => alcove),
+    findAll: jest.fn(async () => [
+      {
+        id: '123',
+      },
+      {
+        id: '456',
+      },
+    ]),
+    findOne: jest.fn(async (id: string) => {
+      return {
+        id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    }),
+    update: jest.fn(async (id: string, update) => {
+      return { id, ...update };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AlcovesController],
       providers: [AlcovesService],
-    }).compile();
+    })
+      .overrideProvider(AlcovesService)
+      .useValue(mockAlcovesService)
+      .compile();
 
     controller = module.get<AlcovesController>(AlcovesController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should list alcoves', async () => {
+    const alcoves = [
+      {
+        id: '123',
+      },
+      {
+        id: '456',
+      },
+    ];
+
+    expect(await controller.findAll()).toEqual({
+      alcoves,
+    });
+  });
+
+  it('should list an alcove', async () => {
+    const alcoveId = '123';
+    expect(await controller.findOne(alcoveId)).toHaveProperty(
+      'alcove.id',
+      alcoveId,
+    );
+  });
+
+  // it('should update an alcove', async () => {
+  //   const updateInput = { name: 'Test' };
+  //   expect(
+  //     await controller.update('123', {
+  //       ...updateInput,
+  //     }),
+  //   ).toEqual({
+  //     id: '123',
+  //     ...updateInput,
+  //   });
+  // });
+
+  it('should create an alcove', async () => {
+    const newAlcove = {
+      id: '456',
+      name: 'New Alcove',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+    expect(await controller.create(newAlcove)).toEqual(newAlcove);
   });
 });
--- pier/src/alcoves/alcoves.controller.ts
+++ pier/src/alcoves/alcoves.controller.ts
@@ -8,9 +8,8 @@ import {
   Controller,
 } from '@nestjs/common';
 import { AlcovesService } from './alcoves.service';
-import { CreateAlcoveDto } from './dto/create-alcove.dto';
-import { UpdateAlcoveDto } from './dto/update-alcove.dto';
 import { CheckAbilities } from '../ability/abilities.decorator';
+import { Alcove } from '@prisma/client';
 
 @Controller('alcoves')
 export class AlcovesController {
@@ -18,31 +17,33 @@ export class AlcovesController {
 
   @Post()
   @CheckAbilities({ action: 'create', subject: 'alcoves' })
-  create(@Body() createAlcoveDto: CreateAlcoveDto) {
-    return this.alcovesService.create(createAlcoveDto);
+  create(@Body() createAlcoveInput: Alcove) {
+    return this.alcovesService.create(createAlcoveInput);
   }
 
   @Get()
   @CheckAbilities({ action: 'read', subject: 'alcoves' })
-  findAll() {
-    return this.alcovesService.findAll();
+  async findAll() {
+    const alcoves = await this.alcovesService.findAll();
+    return { alcoves };
   }
 
   @Get(':id')
   @CheckAbilities({ action: 'read', subject: 'alcoves' })
-  findOne(@Param('id') id: string) {
-    return this.alcovesService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const alcove = await this.alcovesService.findOne(id);
+    return { alcove };
   }
 
   @Patch(':id')
   @CheckAbilities({ action: 'update', subject: 'alcoves' })
-  update(@Param('id') id: string, @Body() updateAlcoveDto: UpdateAlcoveDto) {
-    return this.alcovesService.update(+id, updateAlcoveDto);
+  update(@Param('id') id: string, @Body() updateAlcoveInput: Alcove) {
+    return this.alcovesService.update(id, updateAlcoveInput);
   }
 
   @Delete(':id')
   @CheckAbilities({ action: 'delete', subject: 'alcoves' })
   remove(@Param('id') id: string) {
-    return this.alcovesService.remove(+id);
+    return this.alcovesService.remove(id);
   }
 }
