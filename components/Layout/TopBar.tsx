df40e29d05319e6ab0292b0816d3cb4f480b973f
Brendan Kennedy <brenwken@gmail.com>
2023-04-14 16:17:55 +0000

    Downgrade nextjs

--- components/Layout/TopBar.tsx
+++ components/Layout/TopBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import logo from '../../public/logo.png'
-import { Avatar, Box, Flex, Heading } from '@chakra-ui/react'
+import { Avatar, Flex, Heading } from '@chakra-ui/react'
 import Image from 'next/image'
 
 function Profile() {
