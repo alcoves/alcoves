49431104fc119e1d62d87b0e0f17d8bd1892b832
Brendan Kennedy <brenwken@gmail.com>
2023-12-22 19:00:11 +0000

    Add Sentry for error monitoring and profiling

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,11 @@
+import * as Sentry from '@sentry/node'
+
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
 import { PrismaService } from './services/prisma.service'
 import { NestApplication, NestFactory } from '@nestjs/core'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
@@ -13,6 +16,27 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService)
   const port = configService.get('PORT') || 4000
+  const sentryDsn = configService.get('ALCOVES_SENTRY_DSN')
+
+  if (sentryDsn) {
+    console.log('Sentry DSN found, enabling Sentry')
+    Sentry.init({
+      dsn: sentryDsn,
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Sentry.Integrations.Express({
+          app: app.getHttpAdapter().getInstance(),
+        }),
+        new ProfilingIntegration(),
+      ],
+      tracesSampleRate: 1.0,
+      profilesSampleRate: 1.0,
+    })
+
+    app.use(Sentry.Handlers.requestHandler())
+    app.use(Sentry.Handlers.tracingHandler())
+    app.use(Sentry.Handlers.errorHandler())
+  }
 
   app.enableCors({
     origin: '*',
