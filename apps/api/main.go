e16efecb40f5d9156002a1713d12c8a4c88ffd55
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2024-03-04 10:13:42 -0500

    LF

--- apps/api/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/alcoves/alcoves/apps/api/server"
-
-func main() {
-	app := server.ProductionServer()
-	app.Listen(":4000")
-}
--- apps/client/remix.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  ignoredRouteFiles: ["**/*.css"],
-  tailwind: true,
-  postcss: true,
-};
