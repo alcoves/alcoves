48283d7fcb67db0d683e785cf475dd3b73ce9a59
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2024-03-10 18:27:57 -0400

    Add AccountMenu and SidebarMenu components

---
+++ apps/client/app/components/AccountMenu.tsx
@@ -0,0 +1,46 @@
+import LogoutButton from './auth/logout'
+
+import { UserRecord } from '../services/auth.server'
+import { Button } from './ui/button'
+import { Link } from '@remix-run/react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+
+export default function AccountMenu(props: { user: UserRecord | null }) {
+  if (props.user) {
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar className="w-8 h-8">
+            <AvatarImage src="" />
+            <AvatarFallback>
+              {props?.user?.username?.[0]?.toUpperCase() ?? ''}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogoutButton />
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    )
+  }
+
+  return (
+    <Link to="/login">
+      <Button size="sm" variant="secondary">
+        Log In
+      </Button>
+    </Link>
+  )
+}
---
+++ apps/client/app/components/SidebarMenu.tsx
@@ -0,0 +1,34 @@
+import { Home } from 'lucide-react'
+import { NavLink } from '@remix-run/react'
+import { buttonVariants } from './ui/button'
+import { UserRecord } from '../services/auth.server'
+
+function SidebarLink(props: { to: string; children: React.ReactNode }) {
+  return (
+    <NavLink
+      to={props.to}
+      className={({ isActive, isPending }) => {
+        return `${buttonVariants({
+          variant: isActive ? 'default' : 'ghost',
+        })}`
+      }}
+    >
+      {props.children}
+    </NavLink>
+  )
+}
+
+export default function SidebarMenu(props: { user: UserRecord | null }) {
+  if (props.user) {
+    return (
+      <div className="flex flex-col p-2 w-full">
+        <SidebarLink to="/">
+          <Home className="mr-2 h-4 w-4" />
+          <div className="text-sm">Home</div>
+        </SidebarLink>
+      </div>
+    )
+  }
+
+  return <div>Sidebar</div>
+}
