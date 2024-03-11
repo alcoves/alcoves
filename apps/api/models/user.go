abb8eccdd92f16671420bff0a87174132b2e553e
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2024-03-10 20:07:17 -0400

    Refactor User and UserSession models

--- apps/api/models/user.go
+++ apps/api/models/user.go
@@ -7,14 +7,13 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email" gorm:"uniqueIndex"`
-	// Sessions []UserSession // User has many UserSessions
+	Sessions []UserSession
 }
 
 type UserSession struct {
 	gorm.Model
-	UserID    uint   `json:"user_id"`
+	UserID    uint   `json:"user_id"` // FK
 	SessionID string `json:"session_id" gorm:"uniqueIndex"`
 	IP        string `json:"ip"`
 	UserAgent string `json:"user_agent"`
-	// User      User   // UserSession belongs to a User
 }
