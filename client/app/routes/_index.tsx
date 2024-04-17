f2273f18c78d2851e8d017db406ef7aa74f06e2b
Brendan Kennedy <brenwken@gmail.com>
2024-04-17 00:48:13 -0400

    Dummy data

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,20 +1,38 @@
+import { useLoaderData } from '@remix-run/react'
 import { Button } from '../components/ui/button'
-import type { MetaFunction } from '@remix-run/node'
-import { ModeToggle } from '../components/mode-toggle'
+import { json, type MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
-    return [
-        { title: 'Alcoves' },
-        { name: 'description', content: 'Welcome to Remix!' },
-    ]
+    return [{ title: 'Alcoves' }, { name: 'Alcoves', content: 'Alcoves' }]
+}
+
+interface Video {
+    id: number
+    title: string
+}
+
+export async function loader() {
+    const response = await fetch('http://server:3005/videos')
+    const data = await response.json()
+    const videos: Video[] = data.videos
+    return json({ videos })
 }
 
 export default function Index() {
+    const { videos } = useLoaderData<typeof loader>()
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <Button>Click me</Button>
-            <ModeToggle />
+        <div className="w-screen h-screen">
+            <div className="container max-w-2xl p-4">
+                <div className="flex flex-col items-center justify-center space-y-2">
+                    <h2 className="text-3xl font-bold">Welcome to Alcoves</h2>
+                    <Button size="sm">Upload</Button>
+                    {videos.map((video) => (
+                        <div key={video.id} className="">
+                            {video.title}
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,16 @@ app.get('/', (c) => {
     return c.text('Hello Hono!')
 })
 
+app.get('/videos', (c) => {
+    return c.json({
+        videos: [
+            { id: 1, title: 'Video 1' },
+            { id: 2, title: 'Video 2' },
+            { id: 3, title: 'Video 3' },
+        ],
+    })
+})
+
 app.get('/healthcheck', (c) => {
     return c.text('OK')
 })
