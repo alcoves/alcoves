1813ca781fc468a20efbf3cef45c990611694961
Brendan Kennedy <brenwken@gmail.com>
2024-07-16 05:03:24 -0400

    Styling

--- client/src/components/userAccountMenu.tsx
+++ client/src/components/userAccountMenu.tsx
@@ -1,25 +1,26 @@
-import { Image, IconButton } from '@chakra-ui/react'
-
 import { useAuth } from '../hooks/useAuth'
-import { useNavigate } from 'react-router-dom'
+import { Image, IconButton } from '@chakra-ui/react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export default function UserAccountMenuButton() {
     const navigate = useNavigate()
     const { user } = useAuth()
 
     return (
-        <IconButton
-            size="sm"
-            aria-label="user"
-            onClick={() => navigate('/profile')}
-            icon={
-                <Image
-                    w="100%"
-                    h="100%"
-                    rounded="md"
-                    src={user?.avatar || '/favicon.ico'}
-                />
-            }
-        />
+        <NavLink to="/profile">
+            <IconButton
+                size="sm"
+                aria-label="user"
+                onClick={() => navigate('/profile')}
+                icon={
+                    <Image
+                        w="100%"
+                        h="100%"
+                        rounded="md"
+                        src={user?.avatar || '/favicon.ico'}
+                    />
+                }
+            />
+        </NavLink>
     )
 }
