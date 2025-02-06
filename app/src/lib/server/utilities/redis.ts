b32d364c3641078674e96025115db6ac697f7b34
Brendan Kennedy <9222118+rustyguts@users.noreply.github.com>
2025-02-06 01:58:31 -0500

    Add redis.ts

---
+++ app/src/lib/server/utilities/redis.ts
@@ -0,0 +1,31 @@
+import redis from "redis";
+import { env } from "./env";
+
+export async function getClient() {
+	const client = redis.createClient({
+		url: `redis://${env.ALCOVES_TASK_DB_HOST}:${env.ALCOVES_TASK_DB_PORT}`,
+	});
+
+	client
+		.on("end", () => console.log("Redis client disconnected"))
+		.on("connect", () => console.log("Redis client connecting..."))
+		.on("error", (err) => console.error("Redis Client Error:", err))
+		.on("ready", () => console.log("Redis client connected successfully"))
+		.on("reconnecting", () => console.log("Redis client reconnecting..."));
+
+	process.on("SIGTERM", async () => {
+		await client.quit();
+	});
+
+	process.on("SIGINT", async () => {
+		await client.quit();
+	});
+
+	await client.connect();
+	return client;
+}
+
+export async function pubClient() {
+	const client = await getClient();
+	return client;
+}
